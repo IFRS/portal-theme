4458213ab254109d2a02f04c6309b8ea8e990440
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-05 15:26:25 -0200

    Habilitação de todas as funções disponíveis.

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+// Registra os menus
+require_once('lib/menus.php');
+
+// Fix SSL
+require_once('lib/ssl-fix.php');
+
+// Custom Header
+require_once('lib/custom-header.php');
+
+// Post Thumbnail
+require_once('lib/post-thumbnails.php');
+
+// Breadcrumb
+require_once('lib/breadcrumb.php');
+
+// Script Condicional
+require_once('lib/script_conditional.php');
+
+// Scripts & Styles
+require_once('lib/assets.php');
+
+// Adicionar PrettyPhoto automaticamente.
+require_once('lib/prettyphoto_rel.php');
+
+// Paginação personalizada
+require_once('lib/pagination.php');
+
+// Filtro para buscas vazias
+require_once('lib/empty-search-filter.php');
