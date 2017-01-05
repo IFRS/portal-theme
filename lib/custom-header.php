b8108684ab9b62be31733a6b058842fabc699081
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-05 15:26:04 -0200

    Habilitação e configuração da imagem de cabeçalho personalizada.

---
+++ lib/custom-header.php
@@ -0,0 +1,16 @@
+<?php
+// Habilita a personalização de cabeçalho
+add_theme_support('custom-header', array(
+    'default-image'          => '',
+	'width'                  => 555,
+	'height'                 => 300,
+	'flex-height'            => false,
+	'flex-width'             => false,
+	'uploads'                => true,
+	'random-default'         => false,
+	'header-text'            => false,
+	'default-text-color'     => '',
+	'wp-head-callback'       => '',
+	'admin-head-callback'    => '',
+	'admin-preview-callback' => '',
+));
