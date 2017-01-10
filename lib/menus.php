b147ac880f6ea3ba1089b71e4768e600e6ba3263
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-10 16:12:36 -0200

    Registro dos menus.

--- lib/menus.php
+++ lib/menus.php
@@ -2,6 +2,10 @@
 // Menu Principal
 register_nav_menus(
     array(
-        'main' => 'Menu Principal',
+        'social'     => __('Menu Social'),
+        'servicos'   => __('Barra de Serviços'),
+        'relevancia' => __('Menu de Relevância'),
+        'principal'  => __('Menu Principal'),
+        'rodape'     => __('Menu do Rodapé')
     )
 );
