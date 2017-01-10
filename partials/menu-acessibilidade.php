59593bdbde26e08b76bf291048597c47334f0fc4
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-10 16:14:00 -0200

    Criação dos menus do header.

---
+++ partials/menu-acessibilidade.php
@@ -0,0 +1,5 @@
+<ul id="menu-acessibilidade" class="barra">
+    <li><a href="#"><?php _e('Acessibilidade'); ?></a></li>
+    <li><a href="#"><?php _e('Alto Contraste'); ?></a></li>
+    <li><a href="#"><?php _e('Mapa do Site'); ?></a></li>
+</ul>
---
+++ partials/menu-servicos.php
@@ -0,0 +1,12 @@
+<?php
+wp_nav_menu(
+    array(
+        'menu_class'        => '',
+        'menu_id'           => 'menu-servicos',
+        'container'         => 'nav',
+        'container_class'   => '',
+        'container_id'      => 'menu-servicos-nav',
+        'depth'             => 1,
+        'theme_location'    => 'servicos'
+    )
+);
