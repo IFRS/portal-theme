0735239a7d66089a54c0fffe5edcbab447acd7d3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-10 16:14:43 -0200

    Mudança do botão para buscas, para ficar mais neutro no layout.

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="search-field form-control" placeholder="Buscar no portal" required>
         <span class="input-group-btn">
-            <button type="submit" class="search-submit btn btn-info" title="Buscar"><span class="glyphicon glyphicon-search"></span></button>
+            <button type="submit" class="search-submit btn btn-default" title="Buscar"><span class="glyphicon glyphicon-search"></span></button>
         </span>
     </div>
 </form>
