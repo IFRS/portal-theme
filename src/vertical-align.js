c285c2c5ddee658f29ee0b95efc7bd0601980602
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-10 16:21:02 -0200

    Criação de script para controlar os elementos que precisam de alinhamento vertical.

---
+++ src/vertical-align.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#menu-servicos-nav').flexVerticalCenter({
+        cssAttribute: 'padding-top',
+        parentSelector: '#barra-servicos'
+    });
+    $('#menu-relevancia > li > a').flexVerticalCenter({
+        cssAttribute: 'padding-top',
+    });
+    // $('#menu-principal .sub-menu > li > a').flexVerticalCenter({
+    //     cssAttribute: 'padding-top',
+    // });
+});
