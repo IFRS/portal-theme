5d6d6d8c2f78b94e4ac0029aaab870e62f74e87d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-10 16:13:26 -0200

    Renomeação da barra de atralhos e adição de indicadores.

---
+++ partials/menu-atalhos.php
@@ -0,0 +1,6 @@
+<ul id="menu-atalhos" class="barra">
+    <li><a href="#inicio-conteudo"><?php _e('Ir para o conteúdo'); ?>&nbsp;<span class="badge">1</span></a></li>
+    <li><a href="#inicio-menu"><?php _e('Ir para o menu'); ?>&nbsp;<span class="badge">2</span></a></li>
+    <li><a href="#search-field"><?php _e('Ir para a busca'); ?>&nbsp;<span class="badge">3</span></a></li>
+    <li><a href="#inicio-rodape"><?php _e('Ir para o rodapé'); ?>&nbsp;<span class="badge">4</span></a></li>
+</ul>
