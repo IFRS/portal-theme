8aa6753f07cc54157b4341bee2ef7d0289e1c67c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-13 15:20:55 -0200

    Resgistro do menu de acessibilidade.

--- lib/menus.php
+++ lib/menus.php
@@ -2,10 +2,11 @@
 // Menu Principal
 register_nav_menus(
     array(
-        'social'     => __('Menu Social'),
-        'servicos'   => __('Barra de Serviços'),
-        'relevancia' => __('Menu de Relevância'),
-        'principal'  => __('Menu Principal'),
-        'rodape'     => __('Menu do Rodapé')
+        'acessibilidade' => __('Barra de Acessibilidade'),
+        'social'         => __('Menu Social'),
+        'servicos'       => __('Barra de Serviços'),
+        'relevancia'     => __('Menu de Relevância'),
+        'principal'      => __('Menu Principal'),
+        'rodape'         => __('Menu do Rodapé')
     )
 );
