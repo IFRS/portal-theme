a87602202cac2ac20eeecea7a0d4dd5fbcefe3ef
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-13 15:21:28 -0200

    Ajuste no  menu de acessibilidade para usar o menu dinâmico.

--- partials/menu-acessibilidade.php
+++ partials/menu-acessibilidade.php
@@ -1,5 +1,12 @@
-<ul id="menu-acessibilidade" class="barra">
-    <li><a href="#"><?php _e('Acessibilidade'); ?></a></li>
-    <li><a href="#"><?php _e('Alto Contraste'); ?></a></li>
-    <li><a href="#"><?php _e('Mapa do Site'); ?></a></li>
-</ul>
+<?php
+wp_nav_menu(
+    array(
+        'menu_class'        => 'barra',
+        'menu_id'           => 'menu-acessibilidade',
+        'container'         => 'nav',
+        'container_class'   => '',
+        'container_id'      => '',
+        'depth'             => 1,
+        'theme_location'    => 'acessibilidade'
+    )
+);
