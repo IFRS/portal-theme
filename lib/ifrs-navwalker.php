bf204da2c1d0467777af528abd0cf041255fdac8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-17 09:57:44 -0200

    Adição de ícone indicador de sub-itens nos menus.

--- lib/ifrs-navwalker.php
+++ lib/ifrs-navwalker.php
@@ -2,6 +2,11 @@
 class wp_ifrs_navwalker extends Walker_Nav_Menu {
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
 		$indent = str_repeat( "\t", $depth );
-		$output .= "\n$indent<ul role=\"menu\" class=\" sub-menu collapse\">\n";
+
+		if ($args->walker->has_children) {
+            $output .='<span class="glyphicon glyphicon-chevron-right"></span>';
+        }
+
+		$output .= "\n$indent<ul role=\"menu\" class=\"sub-menu collapse\">\n";
 	}
 }
