787b1ebcd29e2c729f4603e2be69f3ab00344dd8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-17 09:55:56 -0200

    Carregamento do Slicknav e ajustes gerais.

--- lib/assets.php
+++ lib/assets.php
@@ -5,9 +5,11 @@ function load_styles_portal() {
     if (WP_DEBUG) {
         wp_enqueue_style('css-ifrs-portal-theme', get_stylesheet_directory_uri().'/css/ifrs-portal-theme.css', array(), false, 'all');
         wp_enqueue_style( 'css-prettyPhoto', get_stylesheet_directory_uri().'/vendor/prettyPhoto/css/prettyPhoto.css', array(), false, 'screen' );
+        wp_enqueue_style( 'css-slicknav', get_stylesheet_directory_uri().'/vendor/slicknav/dist/slicknav.css', array(), false, 'screen' );
     } else {
         wp_enqueue_style('css-ifrs-portal-theme', get_stylesheet_directory_uri().'/css/ifrs-portal-theme.min.css', array(), false, 'all');
         wp_enqueue_style( 'css-prettyPhoto', get_stylesheet_directory_uri().'/vendor/prettyPhoto/css/prettyPhoto.css', array(), false, 'screen' );
+        wp_enqueue_style( 'css-slicknav', get_stylesheet_directory_uri().'/vendor/slicknav/dist/slicknav.min.css', array(), false, 'screen' );
     }
 }
 
@@ -44,6 +46,9 @@ function load_scripts_portal() {
 
         wp_enqueue_script( 'masonry-custom', get_template_directory_uri().'/vendor/masonry/dist/masonry.pkgd.js', array(), false, true );
         wp_enqueue_script( 'masonry-config', get_template_directory_uri().'/src/masonry-config.js', array('masonry-custom'), false, true );
+
+        wp_enqueue_script( 'slicknav', get_template_directory_uri().'/vendor/slicknav/dist/jquery.slicknav.js', array(), false, true );
+        wp_enqueue_script( 'slicknav-config', get_template_directory_uri().'/src/slicknav-config.js', array('slicknav'), false, true );
     } else {
         wp_enqueue_script( 'html5shiv', get_stylesheet_directory_uri().'/vendor/html5shiv/dist/html5shiv.min.js', array(), false, false );
         wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
@@ -63,13 +68,16 @@ function load_scripts_portal() {
         wp_enqueue_script('jquery-flex-vertical', get_stylesheet_directory_uri().'/vendor/jQuery-Flex-Vertical-Center/jquery.flexverticalcenter.js', array('jquery'), false, true);
         wp_enqueue_script('vertical-align', get_stylesheet_directory_uri().'/js/vertical-align.min.js', array('jquery-flex-vertical'), false, true);
 
-        wp_enqueue_script('menu', get_stylesheet_directory_uri().'/src/menu.min.js', array('bootstrap'), false, true);
+        wp_enqueue_script('menu', get_stylesheet_directory_uri().'/js/menu.min.js', array('bootstrap'), false, true);
 
         wp_enqueue_script( 'jquery-prettyPhoto', get_stylesheet_directory_uri().'/vendor/prettyPhoto/js/jquery.prettyPhoto.js', array('jquery'), false, true );
         wp_enqueue_script('prettyPhoto-config', get_stylesheet_directory_uri().'/js/prettyPhoto-config.min.js', array(), false, true);
 
         wp_enqueue_script( 'masonry-custom', get_template_directory_uri().'/vendor/masonry/dist/masonry.pkgd.min.js', array(), false, true );
         wp_enqueue_script( 'masonry-config', get_template_directory_uri().'/js/masonry-config.min.js', array('masonry-custom'), false, true );
+
+        wp_enqueue_script( 'slicknav', get_template_directory_uri().'/vendor/slicknav/dist/jquery.slicknav.min.js', array(), false, true );
+        wp_enqueue_script( 'slicknav-config', get_template_directory_uri().'/js/slicknav-config.min.js', array('slicknav'), false, true );
 
         wp_enqueue_script( 'js-barra-brasil', '//barra.brasil.gov.br/barra.js', array(), false, true );
     }
