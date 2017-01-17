7931a8df49dd592be5dd65a403b0665def6dd0cb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-17 14:22:26 -0200

    Registro de nova área de widgets na página inicial.

--- lib/widgets.php
+++ lib/widgets.php
@@ -1,11 +1,20 @@
 <?php
 function portal_widgets_init() {
 	register_sidebar(array(
-		'name' => 'Widget Navegação',
-		'id' => 'widget-nav',
-		'description' => 'widget acima do Menu de Relevância.',
+		'name'          => 'Widget Navegação',
+		'id'            => 'widget-nav',
+		'description'   => 'Área acima do Menu de Relevância.',
 		'before_widget' => '<div id="%1$s" class="widget widget-nav visible-md visible-lg %2$s">',
 		'after_widget'  => '</div>',
+		'before_title'  => '<span class="sr-only">',
+		'after_title'   => '</span>',
+	));
+	register_sidebar(array(
+		'name'          => 'Widget Home',
+		'id'            => 'widget-home',
+		'description'   => 'Área ao lado da notícia destaque na página inicial.',
+		'before_widget' => '<div class="row"><div class="col-xs-12"><div id="%1$s" class="widget widget-home %2$s">',
+		'after_widget'  => '</div></div></div>',
 		'before_title'  => '<span class="sr-only">',
 		'after_title'   => '</span>',
 	));
