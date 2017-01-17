ec1a0c0b2b828c251dfc362c628fec763b79c1b4
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-17 09:58:38 -0200

    Adição de lógica para trocar o ícone de indicação de sub-itens nos menus.

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
         toggle: false
     });
     $('.menu-collapse .menu-item-has-children > a').on('click', function(e) {
+        $(this).nextAll('.collapse').collapse('toggle');
         e.preventDefault();
-        $(this).next('.collapse').collapse('toggle');
     });
+
+    $('.collapse').on('show.bs.collapse', function() {
+        $(this).prevAll('.glyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
+    });
+
+    $('.collapse').on('hide.bs.collapse', function() {
+        $(this).prevAll('.glyphicon').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+    });
+
     $('.current-menu-item').parents('.collapse').collapse('show');
 });
