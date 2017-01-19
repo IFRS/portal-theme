a7fdc57d1951257516a2b55e4165a52149a68261
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-01-19 15:25:02 -0200

    Habilitação do masonry na listagem de posts.

--- src/masonry-config.js
+++ src/masonry-config.js
@@ -3,4 +3,9 @@ $(window).load(function() {
         itemSelector: '#menu-rodape > .menu-item',
         percentPosition: true
     });
+
+    $('#lista-noticias').masonry({
+        itemSelector: '#lista-noticias > div',
+        percentPosition: true
+    });
 });
