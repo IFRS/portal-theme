7aa7341526db93ecfc09d52caf45cbe9235dab9f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-06 14:30:44 -0300

    Favicons estáticos, não serão mais gerados automaticamente no build.

---
+++ partials/favicons.php
@@ -0,0 +1,12 @@
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/manifest.json">
+<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/safari-pinned-tab.svg" color="#195128">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon.ico">
+<meta name="msapplication-TileColor" content="#00a300">
+<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/favicons/mstile-144x144.png">
+<meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/favicons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
