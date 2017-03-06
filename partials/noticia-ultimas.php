e0362e1f3f5353e8b2161efcbe7def1d108b3bc6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-06 17:43:39 -0300

    Criação preliminar de partial para mostrar últimas notícias (posts).

---
+++ partials/noticia-ultimas.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-xs-12">
+        <?php
+            global $post;
+
+            $args = array(
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'post_type' => 'post',
+                'numberposts' => 10,
+                'post__not_in' => array($post->ID)
+            );
+
+            $last_posts = get_posts($args);
+        ?>
+        <?php if (!empty($last_posts)) : ?>
+            <div class="well latest-post">
+                <h3>&Uacute;ltimas Not&iacute;cias</h3>
+                <?php foreach ($last_posts as $post) : ?>
+                    <h4 class="latest-post-title"><a href="<?php echo get_permalink($post->ID); ?>" rel="bookmark"><?php echo $post->post_title; ?></a></h4>
+                    <p class="latest-post-date"><?php echo get_the_date('d/m/Y', $post->ID); ?></p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
