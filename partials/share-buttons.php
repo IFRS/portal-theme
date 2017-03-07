5f7e46b76363996c5044016efcc677b14f76a7d1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-07 18:00:28 -0300

    Reordenação dos botões de compartilhamento e ajustes na estilização.

--- partials/share-buttons.php
+++ partials/share-buttons.php
@@ -13,13 +13,13 @@ $crunchifyThumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post-
 $facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$crunchifyURL;
 $twitterURL = 'https://twitter.com/intent/tweet?text='.$crunchifyTitle.'&amp;url='.$crunchifyURL;
 $googleURL = 'https://plus.google.com/share?url='.$crunchifyURL;
-$whatsappURL = 'whatsapp://send?text='.$crunchifyTitle . ' ' . $crunchifyURL;
 $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$crunchifyURL.'&amp;media='.$crunchifyThumbnail[0].'&amp;description='.$crunchifyTitle;
+$whatsappURL = 'whatsapp://send?text='.$crunchifyTitle . ' ' . $crunchifyURL;
 ?>
 <ul class="crunchify-social">
     <li><a class="crunchify-link crunchify-facebook" href="<?php echo $facebookURL; ?>" target="_blank">Facebook</a></li>
     <li><a class="crunchify-link crunchify-twitter" href="<?php echo $twitterURL; ?>" target="_blank">Twitter</a></li>
-    <li><a class="crunchify-link crunchify-whatsapp" href="<?php echo $whatsappURL; ?>" target="_blank">WhatsApp</a></li>
     <li><a class="crunchify-link crunchify-googleplus" href="<?php echo $googleURL; ?>" target="_blank">Google+</a></li>
     <li><a class="crunchify-link crunchify-pinterest" href="<?php echo $pinterestURL; ?>" data-pin-custom="true" target="_blank">Pin It</a></li>
+    <li><a class="crunchify-link crunchify-whatsapp" href="<?php echo $whatsappURL; ?>" target="_blank">WhatsApp</a></li>
 </ul>
