29b4bcf4c64d59ed40697e86761257a01b371594
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-07 18:01:08 -0300

    Ajustes nas configurações.

--- lib/category-single-term.php
+++ lib/category-single-term.php
@@ -3,7 +3,7 @@
 $single_term_campus = new Taxonomy_Single_Term( 'category' );
 $single_term_campus->set( 'priority', 'default' );
 // $single_term_campus->set( 'context', 'normal' );
-// $single_term_campus->set( 'metabox_title', __( 'Custom Metabox Title', 'ifrs-ps-theme' ) );
+// $single_term_campus->set( 'metabox_title', __( 'Custom Metabox Title', 'ifrs-portal-theme' ) );
 $single_term_campus->set( 'force_selection', true );
-$single_term_campus->set( 'indented', false );
+$single_term_campus->set( 'indented', true );
 $single_term_campus->set( 'allow_new_terms', false );
