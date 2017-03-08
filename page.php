d46ca0622f4402ef6328055fd4eb14863c23c336
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-08 17:51:38 -0300

    Ajuste na posição dos links de compartilhamento e adição de menu para subpáginas.

--- page.php
+++ page.php
@@ -10,12 +10,16 @@
                     <h2 class="post-title"><?php the_title(); ?></h2>
                 </div>
             </div>
-            <hr>
-            <div class="row">
-                <div class="col-xs-12">
-                    <?php get_template_part('partials/share', 'buttons'); ?>
-                </div>
-            </div>
+            <?php $children = get_pages(array('child_of' => get_the_ID())); ?>
+            <?php if (count($children) > 0) : ?>
+                <ul class="nav nav-tabs">
+                    <?php foreach ($children as $child): ?>
+                        <li role="presentation"><a href="<?php echo get_page_link($child->ID); ?>"><?php echo $child->post_title; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else : ?>
+                <hr>
+            <?php endif; ?>
             <div class="row">
                 <div class="col-xs-12">
                     <div class="post-content">
@@ -26,6 +30,11 @@
                         ?>
                         <?php the_content(); ?>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <?php get_template_part('partials/share', 'buttons'); ?>
                 </div>
             </div>
         </article>
