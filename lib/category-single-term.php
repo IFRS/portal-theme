159f8cb9e7d9b37d596c3e8b194f29e645b29adf
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-08 17:47:02 -0300

    Correção no nome da variável e colocação do termo apropriado no título do painel das "categorias".

--- lib/category-single-term.php
+++ lib/category-single-term.php
@@ -1,9 +1,9 @@
 <?php
 // Single Term
-$single_term_campus = new Taxonomy_Single_Term( 'category' );
-$single_term_campus->set( 'priority', 'default' );
-// $single_term_campus->set( 'context', 'normal' );
-// $single_term_campus->set( 'metabox_title', __( 'Custom Metabox Title', 'ifrs-portal-theme' ) );
-$single_term_campus->set( 'force_selection', true );
-$single_term_campus->set( 'indented', true );
-$single_term_campus->set( 'allow_new_terms', false );
+$single_term_category = new Taxonomy_Single_Term( 'category' );
+$single_term_category->set( 'priority', 'high' );
+// $single_term_category->set( 'context', 'normal' );
+$single_term_category->set( 'metabox_title', __( 'Cartola', 'ifrs-portal-theme' ) );
+$single_term_category->set( 'force_selection', true );
+$single_term_category->set( 'indented', true );
+$single_term_category->set( 'allow_new_terms', false );
