de3227b85e963a298806c9ae954ce9d34602ea98
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-09 17:14:37 -0300

    Adição de imagem padrão para o cabeçalho.

--- lib/theme-support.php
+++ lib/theme-support.php
@@ -3,9 +3,18 @@ function portal_theme_features()  {
     // Habilita títulos automáticos
     add_theme_support('title-tag');
 
+    // Registra a imagem padrão para o cabeçalho
+    register_default_headers(array(
+        'default-image' => array(
+            'url'           => '%s/img/header-default.png',
+            'thumbnail_url' => '%s/img/header-default-thumb.png',
+            'description'   => __('Imagem de Cabeçalho Padrão', 'ifrs-portal-theme')
+        ),
+    ));
+
     // Habilita a personalização da imagem de cabeçalho
     add_theme_support('custom-header', array(
-        'default-image'          => '',
+        'default-image'          => get_template_directory_uri() . '/img/header-default.png',
         'width'                  => 350,
         'height'                 => 120,
         'flex-width'             => true,
