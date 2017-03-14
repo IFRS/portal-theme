1fbbd099ae7a550691572b5b1840826f476a1385
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-14 17:17:44 -0300

    Aplicação da mesma query de Edital para Categoria de Edital.

--- lib/custom-queries.php
+++ lib/custom-queries.php
@@ -1,7 +1,7 @@
 <?php
 function portal_custom_queries( $query ) {
     if (!is_admin() && $query->is_main_query()) {
-        if ($query->is_post_type_archive('edital')) {
+        if ($query->is_post_type_archive('edital') || $query->is_tax('edital_category')) {
             $query->set('posts_per_page', -1);
             $query->set('nopaging', true);
             $query->set('orderby', 'modified');
