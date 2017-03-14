36e51f612ab853887e722c347ae31fd3ee8e3dd9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-14 17:17:25 -0300

    Ajustes no template de Edital e de Categoria de Edital. Separação do formulário de busca para um partial.

--- archive-edital.php
+++ archive-edital.php
@@ -4,7 +4,7 @@
     <div class="col-xs-12 col-md-9">
         <h2 class="title-box">Editais<?php if (is_search() && get_search_query()) : ?><small>&nbsp;(Resultados da busca por &ldquo;<?php echo get_search_query(); ?>&rdquo;)</small><?php endif; ?></h2>
         <?php if (have_posts()) : ?>
-            <?php get_template_part('partials/edital', 'list'); ?>
+            <?php get_template_part('partials/edital-list'); ?>
         <?php else : ?>
 
                 <?php if (is_search()) : ?>
@@ -13,7 +13,7 @@
                     </div>
                 <?php else : ?>
                     <div class="alert alert-warning" role="alert">
-                        <p><strong>Aguarde!</strong>&nbsp;<?php _e('N&atilde;o h&aacute; Editais publicados.'); ?></p>
+                        <p><strong>Aguarde!</strong>&nbsp;<?php _e('Ainda n&atilde;o h&aacute; Editais publicados.'); ?></p>
                     </div>
                 <?php endif; ?>
         <?php endif; ?>
@@ -21,15 +21,7 @@
     <div class="col-xs-12 col-md-3">
         <aside>
             <?php get_template_part('partials/edital-category', 'list'); ?>
-            <form class="inline-form" method="get" action="<?php echo get_post_type_archive_link( 'edital' ); ?>" role="form">
-                <div class="input-group">
-                    <label class="sr-only" for="s">Termo da busca</label>
-                    <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="s" placeholder="Digite sua busca..."/>
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-primary">Buscar</button>
-                    </span>
-                </div>
-            </form>
+            <?php get_template_part('partials/edital-search'); ?>
         </aside>
     </div>
 </div>
--- taxonomy-edital_category.php
+++ taxonomy-edital_category.php
@@ -4,33 +4,25 @@
     <div class="col-xs-12 col-md-9">
         <h2 class="title-box">Editais na categoria <?php single_term_title(); ?><?php if (is_search() && get_search_query()) : ?><small>&nbsp;(Resultados da busca por &ldquo;<?php echo get_search_query(); ?>&rdquo;)</small><?php endif; ?></h2>
         <?php if (have_posts()) : ?>
-            <?php get_template_part('partials/edital', 'list'); ?>
+            <?php get_template_part('partials/edital-list'); ?>
         <?php else : ?>
                 <?php if (is_search()) : ?>
                     <div class="alert alert-danger" role="alert">
-                        <p><?php _e('N&atilde;o foram encontrados Editais na categoria <?php single_term_title(); ?> com os termos buscados.'); ?></p>
+                        <p><?php printf(__('N&atilde;o foram encontrados Editais na categoria %s com os termos buscados.'), single_term_title('', false)); ?></p>
                     </div>
                 <?php else : ?>
                     <div class="alert alert-warning" role="alert">
-                        <p><strong>Aguarde!</strong>&nbsp;<?php _e('N&atilde;o h&aacute; Editais publicados na categoria <?php single_term_title(); ?>.'); ?></p>
+                        <p><strong>Aguarde!</strong>&nbsp;<?php printf(__('Ainda n&atilde;o h&aacute; Editais publicados na categoria %s .'), single_term_title('', false)); ?></p>
                     </div>
                 <?php endif; ?>
         <?php endif; ?>
     </div>
     <div class="col-xs-12 col-md-3">
         <aside>
-            <?php get_template_part('partials/edital-category', 'list'); ?>
-            <form class="inline-form" method="get" action="<?php echo get_post_type_archive_link( 'edital' ); ?>" role="form">
-                <div class="input-group">
-                    <label class="sr-only" for="s">Termo da busca</label>
-                    <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="s" placeholder="Digite sua busca..."/>
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-primary">Buscar</button>
-                    </span>
-                </div>
-            </form>
+            <?php get_template_part('partials/edital-category-list'); ?>
+            <?php get_template_part('partials/edital-search'); ?>
             <br>
-            <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;Todos os Editais</a>
+            <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="btn btn-default btn-block"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<?php _e('Todos os Editais'); ?></a>
         </aside>
     </div>
 </div>
