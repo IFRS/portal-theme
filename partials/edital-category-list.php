43b1c4ce37f40e3bf8c1436180cb1e00e536b724
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-15 13:37:18 -0300

    Colocação das informações na mesma marcação ao invés de em dias linhas separadas.

--- partials/edital-category-list.php
+++ partials/edital-category-list.php
@@ -1,23 +1,18 @@
 <?php
 $terms = wp_list_categories(array(
-    'echo' => 0,
+    'echo' => false,
     'title_li' => '',
     'taxonomy' => 'edital_category',
     'hide_empty' => false,
 ));
 ?>
 
-<div class="well">
-    <div class="row">
-        <div class="col-xs-12">
-            <h3><?php _e('Categorias'); ?></h3>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <ul class="edital-category-list">
-                <?php echo $terms; ?>
-            </ul>
-        </div>
+
+<div class="row">
+    <div class="col-xs-12">
+        <h3 class="title-box"><?php _e('Categorias'); ?></h3>
+        <ul class="edital-category-list">
+            <?php echo $terms; ?>
+        </ul>
     </div>
 </div>
