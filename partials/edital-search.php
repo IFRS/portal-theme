acabfea80510077b655ac9b89473a85233a7ed14
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-15 13:40:53 -0300

    Correção no ID do campo de busca de Editais para não conflitar com a busca geral.

--- partials/edital-search.php
+++ partials/edital-search.php
@@ -1,7 +1,7 @@
 <form class="inline-form" method="get" action="<?php echo get_post_type_archive_link( 'edital' ); ?>" role="form">
     <div class="input-group">
-        <label class="sr-only" for="s"><?php _e('Termo da busca'); ?></label>
-        <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="s" placeholder="<?php _e('Buscar Editais...'); ?>"/>
+        <label class="sr-only" for="sEdital"><?php _e('Termo da busca'); ?></label>
+        <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="sEdital" placeholder="<?php _e('Buscar Editais...'); ?>"/>
         <span class="input-group-btn">
             <button type="submit" class="btn btn-primary"><?php _e('Buscar'); ?></button>
         </span>
