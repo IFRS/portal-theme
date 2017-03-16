1d42c7ec8391ff81d92100b3ec0d023d3ab12cfd
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-16 17:20:25 -0300

    Adição de texto de ajuda na busca contextual.

--- partials/search-local.php
+++ partials/search-local.php
@@ -3,11 +3,14 @@
     $context = basename($template, '.php');
 ?>
 <form class="inline-form" method="get" action="." role="form">
-    <div class="input-group">
-        <label class="sr-only" for="search-<?php echo $context; ?>"><?php _e('Termo da busca'); ?></label>
-        <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="search-<?php echo $context; ?>" placeholder="<?php _e('Busca contextual...'); ?>"/>
-        <span class="input-group-btn">
-            <button type="submit" class="btn btn-primary" title="<?php _e('Buscar no contexto'); ?>"><span class="glyphicon glyphicon-search"></span></button>
-        </span>
+    <div class="form-goup">
+        <div class="input-group">
+            <label class="sr-only" for="search-<?php echo $context; ?>"><?php _e('Termo da busca'); ?></label>
+            <input class="form-control" type="text" value="<?php echo (get_search_query() ? get_search_query() : ''); ?>" name="s" id="search-<?php echo $context; ?>" placeholder="<?php _e('Busca contextual...'); ?>"/>
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-primary" title="<?php _e('Buscar no contexto'); ?>"><span class="glyphicon glyphicon-search"></span></button>
+            </span>
+        </div>
+        <p class="help-block"><?php _e('Retorna os resultados somente sobre a p&aacute;gina atual.'); ?></p>
     </div>
 </form>
