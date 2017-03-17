3a318dd4e3219f7add76f93ec8b36c744a33e327
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-17 15:33:49 -0300

    Ajustes e estilização da lista de Concursos.

--- partials/concurso-list.php
+++ partials/concurso-list.php
@@ -1,9 +1,10 @@
+<hr>
 <div class="panel-group" id="accordion-concursos" role="tablist" aria-multiselectable="true">
 <?php while (have_posts()) : the_post(); ?>
-    <div class="panel panel-default">
+    <div class="panel panel-concurso">
         <div class="panel-heading" role="tab" id="heading-<?php the_ID(); ?>">
             <h3 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion-concursos" href="#collapse-<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse-<?php the_ID(); ?>">
+                <a class="accordion-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion-concursos" href="#collapse-<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse-<?php the_ID(); ?>">
                     <?php the_title(); ?>
                 </a>
             </h3>
