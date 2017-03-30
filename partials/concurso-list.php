e84682fb6256de210dabe73f2117be9c067dfee5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-03-30 16:11:13 -0300

    Adição do status do concurso na lista de concursos.

--- partials/concurso-list.php
+++ partials/concurso-list.php
@@ -3,6 +3,10 @@
 <?php while (have_posts()) : the_post(); ?>
     <div class="panel panel-concurso">
         <div class="panel-heading" role="tab" id="heading-<?php the_ID(); ?>">
+        <?php $status = get_the_terms(get_the_ID(), 'concurso_status'); ?>
+        <?php if ($status) : ?>
+            <span class="label label-primary"><?php echo $status[0]->name; ?></span>
+        <?php endif; ?>
             <h3 class="panel-title">
                 <a class="accordion-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion-concursos" href="#collapse-<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse-<?php the_ID(); ?>">
                     <?php the_title(); ?>
