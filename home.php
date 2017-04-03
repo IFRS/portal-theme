a8ce6b9f18bb163e2c2383602d150de06335191e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-04-03 17:37:43 -0300

    Adição de classe para estilizar o título da página.

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 
 <div class="row">
     <div class="col-xs-12">
-        <h2><?php echo get_the_title(get_option( 'page_for_posts' )); ?></h2>
+        <h2 class="title-box"><?php echo get_the_title(get_option( 'page_for_posts' )); ?></h2>
     </div>
 </div>
 
