f31b67aba450905a2c806b9e21422b0696e70fae
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-04-11 18:17:03 -0300

    Adição de últimos editais, widget para galeria, widget para imagem adicional e código para widget do Facebook. Ajustes nas notícias.

--- front-page.php
+++ front-page.php
@@ -33,8 +33,6 @@
     </div>
 </div>
 
-<hr class="separador-noticia"/>
-
 <div class="row">
     <!-- Demais notícias -->
     <?php while ($query->have_posts()) : $query->the_post(); ?>
@@ -51,16 +49,28 @@
 <div class="row">
     <div class="col-xs-12">
         <?php wp_reset_query(); ?>
-        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="btn btn-success btn-sm pull-right"><?php _e('Acesse mais notícias'); ?></a>
+        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="btn btn-link btn-sm pull-right"><?php _e('Acesse mais notícias'); ?></a>
     </div>
 </div>
 
 <div class="row">
-    <div class="col-xs-12 col-md-6">
-        <?php if (!dynamic_sidebar('widget-gallery')) : endif; ?>
+    <div class="col-xs-12 col-md-4">
+        <!-- TODO Agenda -->
+    </div>
+    <div class="col-xs-12 col-md-8">
+        <!-- TODO Últimos Editais -->
+        <?php get_template_part('partials/edital-ultimos'); ?>
     </div>
 </div>
 
-
+<div class="row">
+    <div class="col-xs-12 col-md-8 galeria">
+        <?php if (!dynamic_sidebar('widget-gallery')) : endif; ?>
+    </div>
+    <div class="col-xs-12 col-md-4">
+        <?php if (!dynamic_sidebar('widget-home-side')) : endif; ?>
+        <?php get_template_part('partials/facebook'); ?>
+    </div>
+</div>
 
 <?php get_footer(); ?>
