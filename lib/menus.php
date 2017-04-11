74b277a672fa10ef703c219df965961f7b931e93
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-04-11 18:03:44 -0300

    Adição de novo menu para a listagem dos Campi.

--- lib/menus.php
+++ lib/menus.php
@@ -5,6 +5,7 @@ register_nav_menus(
         'acessibilidade' => __('Barra de Acessibilidade'),
         'social'         => __('Menu Social'),
         'servicos'       => __('Barra de Serviços'),
+        'campi'          => __('Lista de Campi'),
         'relevancia'     => __('Menu de Relevância'),
         'principal'      => __('Menu Principal'),
         'rodape'         => __('Menu do Rodapé')
