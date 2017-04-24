f3742f71b0ba0bf0de059327b80d47df1971edc0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-04-24 16:40:12 -0300

    Criação de um index com alerta de erro caso algum template não tenha sido desenvolvido.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="alert alert-danger">
+    <p><strong><?php _e('Erro!'); ?></strong>&nbsp;<?php _e('Template n&atilde;o encontrado.'); ?></p>
+</div>
+
+<?php get_footer(); ?>
