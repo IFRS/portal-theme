ed5654a6316767fca3adb95d2cb97272030bab76
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-04-26 11:54:11 -0300

    Ajuste na cor da data das notícias da página inicial.

--- front-page.php
+++ front-page.php
@@ -56,7 +56,7 @@
 <div class="row">
     <div class="col-xs-12 col-md-4">
         <!-- TODO Agenda -->
-        <?php get_template_part('partials/eventos/latest'); ?>
+        <?php get_template_part('partials/eventos/next'); ?>
     </div>
     <div class="col-xs-12 col-md-8">
         <!-- TODO Últimos Editais -->
