be0b7f9b6e31b8d2d552e310b8e4e58c779c5ea7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-04 15:36:16 -0300

    Adição de teclas de atalho na barra de atalhos.

--- partials/menus/atalhos.php
+++ partials/menus/atalhos.php
@@ -1,6 +1,6 @@
 <ul id="menu-atalhos" class="barra hidden-xs">
-    <li><a href="#inicio-conteudo"><?php _e('Ir para o conteúdo'); ?>&nbsp;<span class="badge">1</span></a></li>
-    <li><a href="#inicio-menu"><?php _e('Ir para o menu'); ?>&nbsp;<span class="badge">2</span></a></li>
-    <li><a href="#search-field"><?php _e('Ir para a busca'); ?>&nbsp;<span class="badge">3</span></a></li>
-    <li><a href="#inicio-rodape"><?php _e('Ir para o rodapé'); ?>&nbsp;<span class="badge">4</span></a></li>
+    <li><a href="#inicio-conteudo" accesskey="1"><?php _e('Ir para o conteúdo'); ?>&nbsp;<span class="badge">1</span></a></li>
+    <li><a href="#inicio-menu" accesskey="2"><?php _e('Ir para o menu'); ?>&nbsp;<span class="badge">2</span></a></li>
+    <li><a href="#search-field" accesskey="3"><?php _e('Ir para a busca'); ?>&nbsp;<span class="badge">3</span></a></li>
+    <li><a href="#inicio-rodape" accesskey="4"><?php _e('Ir para o rodapé'); ?>&nbsp;<span class="badge">4</span></a></li>
 </ul>
