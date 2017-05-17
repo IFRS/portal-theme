7e6177044044a8859e4aed80444674f6322b8e5d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-05-17 15:36:56 -0300

    Adição de link para baixar o documento diretamente. Link do título vai para o single documento.

--- partials/documentos/loop.php
+++ partials/documentos/loop.php
@@ -6,12 +6,15 @@
                 <p class="documento-time"><span class="glyphicon glyphicon-time"></span>&nbsp;<?php echo get_the_time('G\hi'); ?></p>
             </div>
             <div class="documento-body col-xs-12 col-md-10">
-                <?php $arquivo = rwmb_meta('documento_file', array('limit' => 1)); ?>
                 <?php if (is_post_type_archive('documento')) : ?>
                     <p class="documento-type"><?php echo get_the_terms(get_the_ID(), 'documento_type')[0]->name; ?></p>
                 <?php endif; ?>
-                <h3 class="documento-title"><a href="<?php echo $arquivo[0]['url']; ?>" title="Baixar <?php the_title(); ?>" data-toggle="tooltip" data-placement="left"><?php the_title(); ?></a></h3>
-                <p class="documento-content"><?php the_content(); ?></p>
+                <h3 class="documento-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+                <div class="documento-content">
+                    <?php the_excerpt(); ?>
+                </div>
+                <?php $arquivo = rwmb_meta('documento_file', array('limit' => 1)); ?>
+                <a href="<?php echo $arquivo[0]['url']; ?>" title="Baixar <?php the_title(); ?>" data-toggle="tooltip" data-placement="right"><span class="glyphicon glyphicon-download-alt"></span>&nbsp;Baixar arquivo diretamente</a>
             </div>
         </div>
     </article>
