05cd713cf9f2243eeaf8c9972b2c78c6a9af2f3e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-05 10:32:04 -0300

    Remoção do masonry para o menu do rodapé e ajustes no posicionamento dos itens.

--- src/masonry-config.js
+++ src/masonry-config.js
@@ -1,9 +1,4 @@
 $(window).load(function() {
-    $('#menu-rodape').masonry({
-        itemSelector: '#menu-rodape > .menu-item',
-        percentPosition: true
-    });
-
     $('#lista-noticias').masonry({
         itemSelector: '#lista-noticias > div',
         percentPosition: true
