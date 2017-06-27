775cb495f801cfbba4e2384b78cd933e71b87714
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-27 16:20:12 -0300

    Remoção de JS não utilizado.

--- src/add-rel-to-img-link.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.post-content').find('a[href$=".gif"], a[href$=".jpg"], a[href$=".png"], a[href$=".bmp"]').each(function() {
-        $(this).attr('rel', 'gallery');
-    });
-});
