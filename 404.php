bb2a5bcbd3eb6a09f9235f82749c187e803a0ced
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-07-04 10:43:45 -0300

    Criação de template para erro 404.

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="row">
+    <div class="col-xs-12">
+        <div class="alert alert-danger" role="alert">
+            <p><strong>Ops!&nbsp;</strong>A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
