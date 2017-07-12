651af203f5ed64d6805d5bb34cdf3adf1617f4e8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-07-12 14:16:20 -0300

    Remoção das mídias socias no código, podem ser adicionadas na área de widgets.

--- partials/facebook.php
+++
@@ -1,1 +0,0 @@
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIFRSOficial&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="300" height="500" style="border:none;overflow:hidden" scrolling="no" allowTransparency="true" class="facebook-widget"></iframe>
--- partials/twitter.php
+++
@@ -1,1 +0,0 @@
-<a class="twitter-timeline" data-height="500" data-dnt="true" href="https://twitter.com/IF_RS">Tweets por IFRS</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
