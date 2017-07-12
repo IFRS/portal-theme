b410450f9c760993589dae7d2fa12a3507977be2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-07-12 10:11:20 -0300

    Ajuste nas cores para alguns sistemas mobile e adição de configurações principalmente para iOS.

--- partials/favicons.php
+++ partials/favicons.php
@@ -6,7 +6,10 @@
 <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/manifest.json">
 <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/safari-pinned-tab.svg" color="#195128">
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon.ico">
-<meta name="msapplication-TileColor" content="#00a300">
 <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/favicons/mstile-144x144.png">
 <meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/favicons/browserconfig.xml">
-<meta name="theme-color" content="#ffffff">
+<meta name="msapplication-navbutton-color" content="#195128">
+<meta name="msapplication-TileColor" content="#00a300">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="theme-color" content="#195128">
