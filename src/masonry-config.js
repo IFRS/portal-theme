a5fff6f44b1a3596c084d6d6f4ac696b42408d85
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-07-20 11:03:11 -0300

    Remoção do masonry no widget do Instagram que causava um bug.

--- src/masonry-config.js
+++ src/masonry-config.js
@@ -7,10 +7,6 @@ $(function() {
         percentPosition: true
     });
 
-    $('.instagram-pics').masonry({
-        itemSelector: 'li',
-    });
-
     $('.gallery').masonry({
         itemSelector: 'dl',
         percentPosition: true
