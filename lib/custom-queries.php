2cd7162f57b4295cc085155f46c2e1cdb92baded
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-08-01 16:27:24 -0300

    Ordenação das instruções e adição de uma custom query para Documentos.

--- lib/custom-queries.php
+++ lib/custom-queries.php
@@ -1,13 +1,19 @@
 <?php
 function portal_custom_queries( $query ) {
     if (!is_admin() && $query->is_main_query()) {
-        if ($query->is_post_type_archive('edital') || $query->is_tax('edital_category')) {
+        if ($query->is_post_type_archive('concurso') || $query->is_tax('concurso_status')) {
             $query->set('posts_per_page', -1);
             $query->set('nopaging', true);
             $query->set('orderby', 'modified');
             $query->set('order', 'DESC');
         }
-        if ($query->is_post_type_archive('concurso') || $query->is_tax('concurso_status')) {
+        if ($query->is_post_type_archive('documento') || $query->is_tax('documento_type') || $query->is_tax('documento_origin')) {
+            $query->set('posts_per_page', -1);
+            $query->set('nopaging', true);
+            $query->set('orderby', 'modified');
+            $query->set('order', 'DESC');
+        }
+        if ($query->is_post_type_archive('edital') || $query->is_tax('edital_category')) {
             $query->set('posts_per_page', -1);
             $query->set('nopaging', true);
             $query->set('orderby', 'modified');
