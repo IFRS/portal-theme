8f0b1fc41f14ab88828eb978228a60efc190b3e2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-08-08 10:53:59 -0300

    Alteração das queries de posts. Unificação das queries e limitação de resultados exibidos.

--- front-page.php
+++ front-page.php
@@ -7,56 +7,39 @@
 ?>
 
 <?php
-    $args_sticky = array(
-        'posts_per_page' => 1,
-        'post__in'  => get_option( 'sticky_posts' ),
-        'ignore_sticky_posts' => 1
-    );
-    $query_sticky = new WP_Query( $args_sticky );
-?>
-
-<div class="row">
-    <div class="col-xs-12 col-md-5 col-md-push-7">
-        <!-- Banners -->
-        <?php if (!dynamic_sidebar('widget-home')) : endif; ?>
-    </div>
-    <div class="col-xs-12 col-md-7 col-md-pull-5">
-        <?php if ($query_sticky->have_posts()) : ?>
-            <!-- Notícia Destaque -->
-            <?php
-                $query_sticky->the_post();
-                $sticky_ID = get_the_ID();
-            ?>
-            <article class="noticia-destaque">
-                <?php get_template_part('partials/noticias/item-front-page'); ?>
-            </article>
-        <?php endif; ?>
-    </div>
-</div>
-
-<?php
-    $posts_to_show = 4;
-    $posts_per_page = $posts_to_show;
-    $sticky_number = count(get_option( 'sticky_posts' ));
-    if ($sticky_number > 1 && $sticky_number <= 4) {
-        $posts_per_page = $posts_to_show - ($sticky_number - 1);
-    }
     $args = array(
         'post_type' => 'post',
-        'post__not_in' => array($sticky_ID),
-        'posts_per_page' => $posts_per_page
+        'posts_per_page' => 5
     );
+
     $query = new WP_Query($args);
 ?>
 
 <div class="row">
-    <!-- Demais notícias -->
-    <?php while ($query->have_posts()) : $query->the_post(); ?>
-        <div class="col-xs-12 col-sm-6 col-md-3">
-            <article class="noticia">
-                <?php get_template_part('partials/noticias/item-front-page'); ?>
-            </article>
-        </div>
+    <!-- Banners -->
+    <div class="col-xs-12 col-md-5 col-md-push-7">
+        <?php if (!dynamic_sidebar('widget-home')) : endif; ?>
+    </div>
+    <?php while ($query->have_posts() && $query->current_post < 4) : $query->the_post(); ?>
+        <?php if ($query->current_post == 0) : ?>
+            <!-- Notícia Destaque -->
+            <div class="col-xs-12 col-md-7 col-md-pull-5">
+                <article class="noticia-destaque">
+                      <?php get_template_part('partials/noticias/item-front-page'); ?>
+                </article>
+            </div>
+         </div> <!-- /.row  -->
+        <?php else : ?>
+            <?php if ($query->current_post == 1) : ?>
+                <div class="row">
+            <?php endif; ?>
+            <!-- Notícia Normal -->
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <article class="noticia">
+                    <?php get_template_part('partials/noticias/item-front-page'); ?>
+                </article>
+            </div>
+        <?php endif; ?>
     <?php endwhile; ?>
 </div>
 
