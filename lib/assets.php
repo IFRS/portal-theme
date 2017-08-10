8a7aabb6ea18a5cd3a350996cc6ce8ca004b06fb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-08-10 18:10:24 -0300

    Ajustes devido a remoção do pacote critical.

--- lib/assets.php
+++ lib/assets.php
@@ -3,8 +3,6 @@ function portal_load_styles() {
     /* wp_register_style( $handle, $src, $deps, $ver, $media ); */
     /* wp_enqueue_style( $handle[, $src, $deps, $ver, $media] ); */
 
-    wp_enqueue_style('css-critical', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/critical.css' : '/css/critical.min.css'), array(), null, 'all');
-
     wp_enqueue_style('css-vendor', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/vendor.css' : '/css/vendor.min.css'), array(), null, 'all');
 
     wp_enqueue_style('css-app', get_stylesheet_directory_uri().(WP_DEBUG ? '/css/app.css' : '/css/app.min.css'), array('css-vendor'), null, 'all');
