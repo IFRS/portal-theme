24802bb12d0120bb2924f6ab452ee348bd8042b3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-08-25 16:09:48 -0300

    Ajustes na acessibilidade do menu.

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,8 @@
 $(function() {
     $('.menu-collapse .sub-menu').addClass('collapse');
     $('.menu-collapse .sub-menu').prev('a').addClass('collapsed');
+    $('.menu-collapse .sub-menu').prev('a').attr('aria-expanded', 'false');
+    $('.menu-collapse .sub-menu').prev('a').append('<span class="sr-only"> (Expandir submenus)</span>');
 
     $('#menu-main .sub-menu').collapse({
         toggle: false
@@ -13,11 +15,15 @@ $(function() {
     $('.collapse').on('show.bs.collapse', function(e) {
         var collapse = $(e.target);
         collapse.prev('a').removeClass('collapsed');
+        collapse.prev('a').attr('aria-expanded', 'true');
+        collapse.prev('a').children('span.sr-only').first().text(' (Ocultar submenus)');
     });
 
     $('.collapse').on('hide.bs.collapse', function(e) {
         var collapse = $(e.target);
         collapse.prev('a').addClass('collapsed');
+        collapse.prev('a').attr('aria-expanded', 'false');
+        collapse.prev('a').children('span.sr-only').first().text(' (Expandir submenus)');
     });
 
     // Abre todos os collapse até o item atual;
