c7263bc0b0948466940a91157572b01c323e5bdf
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-09-04 16:31:37 -0300

    Ajuste no espaçamento dos meta dados das notícias.

--- partials/noticias/item-front-page.php
+++ partials/noticias/item-front-page.php
@@ -15,7 +15,7 @@
             $cat_name = $categories[0]->cat_name;
             $cat_ID = $categories[0]->term_id;
         ?>
-        <p><?php if (!is_category()) : ?><span class="noticia-cartola"><a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a></span> - <?php endif; ?><span class="noticia-data"><?php echo get_the_date(); ?></span></p>
+        <p class="noticia-meta"><?php if (!is_category()) : ?><span class="noticia-cartola"><a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a></span> - <?php endif; ?><span class="noticia-data"><?php echo get_the_date(); ?></span></p>
     </div>
 </div>
 <div class="row">
