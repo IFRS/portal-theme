a00d978eed052bfae5ca9cd18de10840f8496b04
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-09-06 15:05:01 -0300

    Adição de textos introdutórios para Documentos e Editais. Ajuste nas funções de internacionalização.

--- partials/documentos/documentos.php
+++ partials/documentos/documentos.php
@@ -1,6 +1,9 @@
 <div class="row">
     <div class="col-xs-12 col-md-9" id="documentos">
         <h2 class="title"><?php get_template_part('partials/documentos/title'); ?></h2>
+        <p><?php _e('Esta página disponibiliza documentos oficiais emitidos pela Reitoria do IFRS: atas, boletins de pessoal, boletins de serviço, contratos, documentos norteadores da instituição, instruções normativas, planos de ação, políticas, portarias, relatórios e resoluções.', 'ifrs-portal-theme'); ?></p>
+        <p><?php _e('A relação abaixo está organizada por ordem de publicação ou atualização, os mais atuais aparecem primeiro. Mas é possível, também, consultar por categorias. Basta clicar, no menu à direita, no tipo de documento procurado.', 'ifrs-portal-theme'); ?></p>
+        <p><?php _e('Documentos antigos podem ser buscados no site anterior do IFRS, na página do setor ao qual o documento está vinculado.', 'ifrs-portal-theme'); ?></p>
         <?php if (have_posts()) : ?>
             <?php get_template_part('partials/documentos/loop'); ?>
         <?php else : ?>
@@ -10,7 +13,7 @@
                 </div>
             <?php else : ?>
                 <div class="alert alert-warning" role="alert">
-                    <p><strong><?php _e('Aguarde!'); ?></strong>&nbsp;<?php if (is_tax('documento_type')) : _e('Ainda n&atilde;o h&aacute; Documentos deste tipo publicados.'); else : _e('Ainda n&atilde;o h&aacute; Documentos publicados.'); endif; ?></p>
+                    <p><strong><?php _e('Aguarde!', 'ifrs-portal-theme'); ?></strong>&nbsp;<?php if (is_tax('documento_type')) : _e('Ainda n&atilde;o h&aacute; Documentos deste tipo publicados.', 'ifrs-portal-theme'); else : _e('Ainda n&atilde;o h&aacute; Documentos publicados.', 'ifrs-portal-theme'); endif; ?></p>
                 </div>
             <?php endif; ?>
         <?php endif; ?>
@@ -31,7 +34,7 @@
             ?>
             <?php if (is_year() || is_month() || is_tax('documento_type')) : ?>
                 <br>
-                <a href="<?php echo get_post_type_archive_link( 'documento' ); ?>" class="btn btn-default btn-block"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<?php _e('Todos os Documentos'); ?></a>
+                <a href="<?php echo get_post_type_archive_link( 'documento' ); ?>" class="btn btn-default btn-block"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<?php _e('Todos os Documentos', 'ifrs-portal-theme'); ?></a>
             <?php endif; ?>
         </aside>
     </div>
--- partials/editais/editais.php
+++ partials/editais/editais.php
@@ -1,16 +1,23 @@
 <div class="row">
     <div class="col-xs-12 col-md-9" id="editais">
         <h2 class="title"><?php get_template_part('partials/editais/title'); ?></h2>
+        <p><?php _e('Neste espaço, é possível acessar editais publicados pelos diferentes setores da Reitoria do IFRS. A lista de documentos está organizada por ordem de publicação ou atualização. Os mais atuais aparecem primeiro. É possível, também, consultar por categorias. Basta clicar, no menu à direita, no setor responsável pelo edital procurado. Em alguns casos, há ainda categorias relacionadas ao setor, para facilitar as buscas.', 'ifrs-portal-theme'); ?></p>
+        <p><?php _e('Editais antigos podem ser buscados no site anterior do IFRS, na página do setor ao qual o edital está vinculado.', 'ifrs-portal-theme'); ?></p>
+        <p>
+            <em><?php _e('Saiba mais:', 'ifrs-portal-theme'); ?></em><br/>
+            <?php _e('Edital é um documento oficial escrito que contém informações, determinações e orientações sobre: bolsas de ensino, pesquisa e extensão; concursos para processo seletivo de estudantes; concorrências administrativas; concursos para provimento de cargos públicos (para esses, acesse o menu Concursos);  e outros temas que, por sua natureza, devam ter ampla divulgação.', 'ifrs-portal-theme'); ?>
+        </p>
+        <p><?php _e(''); ?></p>
         <?php if (have_posts()) : ?>
             <?php get_template_part('partials/editais/loop'); ?>
         <?php else : ?>
                 <?php if (is_search()) : ?>
                     <div class="alert alert-danger" role="alert">
-                        <p><?php printf(__('N&atilde;o foram encontrados Editais na categoria %s com os termos buscados.'), single_term_title('', false)); ?></p>
+                        <p><?php printf(__('N&atilde;o foram encontrados Editais na categoria %s com os termos buscados.', 'ifrs-portal-theme'), single_term_title('', false)); ?></p>
                     </div>
                 <?php else : ?>
                     <div class="alert alert-warning" role="alert">
-                        <p><strong><?php _e('Aguarde!'); ?></strong>&nbsp;<?php printf(__('Ainda n&atilde;o h&aacute; Editais publicados na categoria %s.'), single_term_title('', false)); ?></p>
+                        <p><strong><?php _e('Aguarde!'); ?></strong>&nbsp;<?php printf(__('Ainda n&atilde;o h&aacute; Editais publicados na categoria %s.', 'ifrs-portal-theme'), single_term_title('', false)); ?></p>
                     </div>
                 <?php endif; ?>
         <?php endif; ?>
@@ -22,7 +29,7 @@
             <?php get_template_part('partials/search-local'); ?>
             <?php if (is_tax('edital_category')) : ?>
                 <br>
-                <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="btn btn-default btn-block"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<?php _e('Todos os Editais'); ?></a>
+                <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="btn btn-default btn-block"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;<?php _e('Todos os Editais', 'ifrs-portal-theme'); ?></a>
             <?php endif; ?>
         </aside>
     </div>
