cb34f16d6f56d0e312193f5044245f97aff39bad
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-10-30 11:43:22 -0200

    Remoção da subordinação no título.

--- partials/header-title.php
+++ partials/header-title.php
@@ -2,8 +2,3 @@
 <?php if (get_header_image() != '') : ?>
     <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" alt="<?php bloginfo('name'); ?>" class="img-responsive"/></a>
 <?php endif; ?>
-<div class="row">
-    <div class="col-xs-12">
-        <p class="title-mec"><?php _e('Ministério da Educação'); ?></p>
-    </div>
-</div>
