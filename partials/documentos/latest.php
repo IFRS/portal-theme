73689594c83cf5c6c77724dcf6d8ccc34c75fb4f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-11-06 14:54:45 -0200

    Ajuste para os últimos editais e documentos somente aparecerem caso tenha algo cadastrado.

--- partials/documentos/latest.php
+++ partials/documentos/latest.php
@@ -1,36 +1,35 @@
+<?php
+    global $post;
+
+    $args = array(
+        'orderby' => 'modified',
+        'order' => 'DESC',
+        'post_type' => 'documento',
+        'posts_per_page' => 5
+    );
+
+    $last_documentos = new WP_Query($args);
+?>
+<?php if ($last_documentos->have_posts()) : ?>
 <div class="row">
     <div class="col-xs-12">
-    <?php
-        global $post;
-
-        $args = array(
-            'orderby' => 'modified',
-            'order' => 'DESC',
-            'post_type' => 'documento',
-            'posts_per_page' => 5
-        );
-
-        $last_documentos = new WP_Query($args);
-    ?>
-
         <div class="latest-documentos">
             <h2 class="title"><?php _e('&Uacute;ltimos Documentos'); ?></h2>
-            <?php if ($last_documentos->have_posts()) : ?>
-                <?php while ($last_documentos->have_posts()) : $last_documentos->the_post(); ?>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <p class="documento-date-time">
-                                <span class="glyphicon glyphicon-calendar"></span>&nbsp;<?php echo get_the_modified_date('d/m/Y'); ?>
-                                &agrave;s
-                                <?php echo get_the_modified_time('G\hi'); ?>
-                            </p>
-                            &bull;
-                            <?php echo get_the_term_list(get_the_ID(), 'documento_type', '<ul class="documento-type"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
-                            <h3 class="documento-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        </div>
+            <?php while ($last_documentos->have_posts()) : $last_documentos->the_post(); ?>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p class="documento-date-time">
+                            <span class="glyphicon glyphicon-calendar"></span>&nbsp;<?php echo get_the_modified_date('d/m/Y'); ?>
+                            &agrave;s
+                            <?php echo get_the_modified_time('G\hi'); ?>
+                        </p>
+                        &bull;
+                        <?php echo get_the_term_list(get_the_ID(), 'documento_type', '<ul class="documento-type"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
+                        <h3 class="documento-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+                </div>
+            <?php endwhile; ?>
+
         </div>
     </div>
 </div>
@@ -41,3 +40,4 @@
         <a href="<?php echo get_post_type_archive_link( 'documento' ); ?>" class="pull-right link-todos-documentos"><?php _e('Acesse todos os Documentos'); ?></a>
     </div>
 </div>
+<?php endif; ?>
--- partials/editais/latest.php
+++ partials/editais/latest.php
@@ -1,36 +1,34 @@
+<?php
+    global $post;
+
+    $args = array(
+        'orderby' => 'modified',
+        'order' => 'DESC',
+        'post_type' => 'edital',
+        'posts_per_page' => 5
+    );
+
+    $last_editais = new WP_Query($args);
+?>
+<?php if ($last_editais->have_posts()) : ?>
 <div class="row">
     <div class="col-xs-12">
-    <?php
-        global $post;
-
-        $args = array(
-            'orderby' => 'modified',
-            'order' => 'DESC',
-            'post_type' => 'edital',
-            'posts_per_page' => 5
-        );
-
-        $last_editais = new WP_Query($args);
-    ?>
-
         <div class="latest-editais">
             <h2 class="title"><?php _e('&Uacute;ltimos Editais'); ?></h2>
-            <?php if ($last_editais->have_posts()) : ?>
-                <?php while ($last_editais->have_posts()) : $last_editais->the_post(); ?>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <p class="edital-date-time">
-                                <span class="glyphicon glyphicon-calendar"></span>&nbsp;<?php echo get_the_modified_date('d/m/Y'); ?>
-                                &agrave;s
-                                <?php echo get_the_modified_time('G\hi'); ?>
-                            </p>
-                            &bull;
-                            <?php echo get_the_term_list(get_the_ID(), 'edital_category', '<ul class="edital-categories"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
-                            <h3 class="edital-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                        </div>
+            <?php while ($last_editais->have_posts()) : $last_editais->the_post(); ?>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p class="edital-date-time">
+                            <span class="glyphicon glyphicon-calendar"></span>&nbsp;<?php echo get_the_modified_date('d/m/Y'); ?>
+                            &agrave;s
+                            <?php echo get_the_modified_time('G\hi'); ?>
+                        </p>
+                        &bull;
+                        <?php echo get_the_term_list(get_the_ID(), 'edital_category', '<ul class="edital-categories"><li>', ',&nbsp;</li><li>', '</li></ul>'); ?>
+                        <h3 class="edital-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+                </div>
+            <?php endwhile; ?>
         </div>
     </div>
 </div>
@@ -41,3 +39,4 @@
         <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>" class="pull-right link-todos-editais"><?php _e('Acesse todos os Editais'); ?></a>
     </div>
 </div>
+<?php endif; ?>
