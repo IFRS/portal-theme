0bd612c20977b479d56c616ea5d3b97fc110b67e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-11-09 15:46:08 -0200

    Adição da tag iframe nos elementos permitidos no Editor.

--- lib/custom-tinymce.php
+++ lib/custom-tinymce.php
@@ -1,12 +1,14 @@
 <?php
 /**
- *  Remove h1 from the WordPress editor.
+ *  Remove h1 and add iframe to the WordPress editor.
  *
  *  @param   array  $init  The array of editor settings
  *  @return  array         The modified edit settings
  */
-function wp_remove_h1_from_editor( $init ) {
-    $init['block_formats'] = 'Paragraph=p;Heading 1=h3;Heading 2=h4;Heading 3=h5;Heading 4=h6;Preformatted=pre;';
-    return $init;
+function portal_custom_tinymce($initArray) {
+    $initArray['block_formats'] = 'Paragraph=p;Heading 1=h3;Heading 2=h4;Heading 3=h5;Heading 4=h6;Preformatted=pre;';
+    $initArray['extended_valid_elements'] = "iframe[id|class|title|style|align|frameborder|height|longdesc|marginheight|marginwidth|name|scrolling|src|width]";
+    return $initArray;
 }
-add_filter( 'tiny_mce_before_init', 'wp_remove_h1_from_editor' );
+
+add_filter('tiny_mce_before_init', 'portal_custom_tinymce');
