c20b6b679d94b254344d534ab8487d605f3f08f9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-04-02 14:21:45 -0300

    Ajuste do meta e do resumo das notícias para que comecem logo abaixo do título.

--- partials/noticias/item.php
+++ partials/noticias/item.php
@@ -6,20 +6,20 @@
             <img src="<?php echo get_template_directory_uri(); ?>/img/noticia-placeholder-<?php echo mt_rand(0, 9); ?>.png" alt="<?php the_title(); ?>" class="img-responsive center-block noticia-imagem"/>
         <?php endif; ?>
         <h2 class="noticia-titulo"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    </div>
-</div>
-<div class="row">
-    <div class="col-xs-12">
         <?php
             $categories = get_the_category();
             $cat_name = $categories[0]->cat_name;
             $cat_ID = $categories[0]->term_id;
         ?>
-        <p class="noticia-meta"><?php if (!is_category()) : ?><span class="noticia-cartola"><a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a></span> - <?php endif; ?><span class="noticia-data"><?php echo get_the_date(); ?></span></p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-xs-12">
+        <p class="noticia-meta">
+        <?php if (!is_category()) : ?>
+            <span class="noticia-cartola">
+                <a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a>
+            </span>
+            -
+        <?php endif; ?>
+            <span class="noticia-data"><?php echo get_the_date(); ?></span>
+        </p>
         <div class="noticia-resumo">
             <?php the_excerpt(); ?>
         </div>
