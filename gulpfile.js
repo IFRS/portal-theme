82a987e33b37c275065eac3d2226765e16508e38
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-06-14 09:05:36 -0300

    Reorganização do arquivo e adição de contextualização nas mensagens de debug

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const argv         = require('minimist')(process.argv.slice(2));
-const gulp         = require('gulp');
+const autoprefixer = require('autoprefixer');
+const browserSync  = require('browser-sync').create();
 const del          = require('del');
+const gulp         = require('gulp');
+const cssmin       = require('gulp-cssmin');
+const debug        = require('gulp-debug');
+const imagemin     = require('gulp-imagemin');
+const postcss      = require('gulp-postcss');
 const rename       = require('gulp-rename');
 const sass         = require('gulp-sass');
-const postcss      = require('gulp-postcss');
-const pixrem       = require('pixrem');
-const autoprefixer = require('autoprefixer');
-const flexibility  = require('postcss-flexibility');
-const cssmin       = require('gulp-cssmin');
 const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const webpack      = require('webpack');
-const PluginError  = require('plugin-error');
-const path         = require('path');
-const browserSync  = require('browser-sync').create();
 const merge2       = require('merge2');
+const argv         = require('minimist')(process.argv.slice(2));
+const path         = require('path');
+const pixrem       = require('pixrem');
+const PluginError  = require('plugin-error');
+const flexibility  = require('postcss-flexibility');
 const through2     = require('through2');
-const debug        = require('gulp-debug');
+const webpack      = require('webpack');
 
-const DIST = [
+const dist = [
     '**',
     '!.**',
     '!dist{,/**}',
@@ -47,7 +47,7 @@ gulp.task('sass', function() {
         outputStyle: 'expanded'
     }).on('error', sass.logError))
     .pipe(postcss(postCSSplugins))
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'SASS:'}) : through2.obj())
     .pipe(gulp.dest('css/'))
     .pipe(browserSync.stream());
 });
@@ -56,7 +56,7 @@ gulp.task('styles', gulp.series('sass', function css() {
     return gulp.src(['css/*.css', '!css/*.min.css'])
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'CSS:'}) : through2.obj())
     .pipe(gulp.dest('css/'))
     .pipe(browserSync.stream());
 }));
@@ -101,22 +101,22 @@ gulp.task('scripts', gulp.series('webpack', function js() {
         mangle: false,
     }))
     .pipe(rename({suffix: '.min'}))
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'JS:'}) : through2.obj())
     .pipe(gulp.dest('js/'))
     .pipe(browserSync.stream());
 }));
 
 gulp.task('assets', function() {
     var open_sans = gulp.src('node_modules/npm-font-open-sans/fonts/**/*')
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'Assets OpenSans:'}) : through2.obj())
     .pipe(gulp.dest('fonts/opensans/'));
 
     var bootstrap = gulp.src('node_modules/bootstrap-sass/assets/fonts/**/*')
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'Assets Bootstrap:'}) : through2.obj())
     .pipe(gulp.dest('fonts/'));
 
     var fancybox = gulp.src('node_modules/jquery-fancybox/source/img/**/*')
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'Assets Fancybox:'}) : through2.obj())
     .pipe(gulp.dest('img/vendor/'));
 
     return merge2(open_sans, bootstrap, fancybox);
@@ -125,13 +125,13 @@ gulp.task('assets', function() {
 gulp.task('images', function() {
     return gulp.src('img/*.{png,jpg,gif}')
     .pipe(imagemin())
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    .pipe((argv.debug) ? debug({title: 'Images:'}) : through2.obj())
     .pipe(gulp.dest('img/'));
 });
 
 gulp.task('dist', function() {
-    return gulp.src(DIST)
-    .pipe((argv.debug) ? debug() : through2.obj() )
+    return gulp.src(dist)
+    .pipe((argv.debug) ? debug({title: 'Dist:'}) : through2.obj())
     .pipe(gulp.dest('dist/'));
 });
 
