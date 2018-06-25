938c7f78f6e62ba4b55f315669163c7b51f158c8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-06-25 15:50:15 -0300

    Ajuste no menu subpages

--- partials/menus/subpages.php
+++ partials/menus/subpages.php
@@ -9,12 +9,12 @@
     $parent = wp_get_post_parent_id( get_the_ID() );
 ?>
 <?php if (count($children) > 0) : ?>
-    <ul class="nav nav-pills nav-subpages">
+    <ul class="nav nav-pills menu-subpages">
         <!-- <?php if ($parent) : ?>
             <li><a href="<?php echo get_page_link($parent); ?>" title="Retornar ao nível anterior"><span class="glyphicon glyphicon-arrow-up"></span></a></li>
         <?php endif; ?> -->
         <?php foreach ($children as $child): ?>
-            <li><a href="<?php echo get_page_link($child->ID); ?>"><?php echo $child->post_title; ?></a></li>
+            <li class="menu-subpages__item"><a class="menu-subpages__link" href="<?php echo get_page_link($child->ID); ?>"><?php echo $child->post_title; ?></a></li>
         <?php endforeach; ?>
     </ul>
 <?php else : ?>
