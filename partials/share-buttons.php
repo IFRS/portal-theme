5d543ba6d7345f4dcd2b9d286883dcfceda8df04
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-06-25 15:50:48 -0300

    Padronização na estilização dos share buttons

--- partials/share-buttons.php
+++ partials/share-buttons.php
@@ -17,9 +17,9 @@ $pinterestURL = 'https://pinterest.com/pin/create/button/?url='.$crunchifyURL.'&
 $whatsappURL = 'whatsapp://send?text='.$crunchifyTitle . ' ' . $crunchifyURL;
 ?>
 <ul class="crunchify-social">
-    <li><a class="crunchify-link crunchify-facebook" href="<?php echo $facebookURL; ?>" target="_blank">Facebook</a></li>
-    <li><a class="crunchify-link crunchify-twitter" href="<?php echo $twitterURL; ?>" target="_blank">Twitter</a></li>
-    <li><a class="crunchify-link crunchify-googleplus" href="<?php echo $googleURL; ?>" target="_blank">Google+</a></li>
-    <li><a class="crunchify-link crunchify-pinterest" href="<?php echo $pinterestURL; ?>" data-pin-custom="true" target="_blank">Pin It</a></li>
-    <li><a class="crunchify-link crunchify-whatsapp" href="<?php echo $whatsappURL; ?>" target="_blank">WhatsApp</a></li>
+    <li class="crunchify-social__item"><a class="crunchify-social__link crunchify-social__link_facebook" href="<?php echo $facebookURL; ?>" target="_blank">Facebook</a></li>
+    <li class="crunchify-social__item"><a class="crunchify-social__link crunchify-social__link_twitter" href="<?php echo $twitterURL; ?>" target="_blank">Twitter</a></li>
+    <li class="crunchify-social__item"><a class="crunchify-social__link crunchify-social__link_googleplus" href="<?php echo $googleURL; ?>" target="_blank">Google+</a></li>
+    <li class="crunchify-social__item"><a class="crunchify-social__link crunchify-social__link_pinterest" href="<?php echo $pinterestURL; ?>" data-pin-custom="true" target="_blank">Pin It</a></li>
+    <li class="crunchify-social__item"><a class="crunchify-social__link crunchify-social__link_whatsapp" href="<?php echo $whatsappURL; ?>" target="_blank">WhatsApp</a></li>
 </ul>
