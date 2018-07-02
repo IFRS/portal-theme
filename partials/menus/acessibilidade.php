a24a15ef427deef395a9e149f8550924b3f75b43
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-07-02 10:01:01 -0300

    Ajustes na marcação dos menus

--- partials/menus/acessibilidade.php
+++ partials/menus/acessibilidade.php
@@ -2,10 +2,10 @@
 wp_nav_menu(
     array(
         'menu_class'        => 'menu-acessibilidade',
-        'menu_id'           => '',
+        'menu_id'           => false,
         'container'         => 'nav',
-        'container_class'   => '',
-        'container_id'      => '',
+        'container_class'   => false,
+        'container_id'      => false,
         'depth'             => 1,
         'theme_location'    => 'acessibilidade'
     )
