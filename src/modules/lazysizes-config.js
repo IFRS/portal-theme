36ca535e2efddfde07f1b67e05bf192719715ca1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-10 11:38:23 -0300

    Ajuste na configuração do lazyload de imagens

--- src/modules/lazysizes-config.js
+++ src/modules/lazysizes-config.js
@@ -3,7 +3,7 @@ require('lazysizes');
 window.lazySizesConfig = window.lazySizesConfig || {};
 window.lazySizesConfig.srcAttr = 'src';
 window.lazySizesConfig.srcsetAttr = 'srcset';
-window.lazySizesConfig.loadMode = 1;
+
 $(function() {
     $('img[srcset]').attr('data-sizes', 'auto');
     $('img[srcset]').addClass('lazyload');
