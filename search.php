6beb9bc1ce2f4c1eaf38f3701d74dc8b767bbe07
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-24 16:41:24 -0300

    Incorporação do partial com um resultado de busca no template principal

--- search.php
+++ search.php
@@ -11,7 +11,17 @@
         </div>
     </div>
     <?php while (have_posts()) : the_post(); ?>
-        <?php get_template_part('partials/search-result'); ?>
+    <div class="row search-result">
+        <div class="col-xs-12">
+            <h3 class="search-result__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        </div>
+        <div class="col-xs-12">
+            <p class="search-result__link"><?php the_permalink(); ?></p>
+        </div>
+        <div class="col-xs-12">
+            <?php the_excerpt(); ?>
+        </div>
+    </div>
     <?php endwhile; ?>
 <?php endif; ?>
 
