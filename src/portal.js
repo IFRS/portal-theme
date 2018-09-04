5b19cd02b4d0eb663fca4fe8417385a0aa63d43a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-04 13:38:11 -0300

    Remoção do plugin masonry

--- src/portal.js
+++ src/portal.js
@@ -4,6 +4,5 @@ require('./modules/datatables-config');
 require('./modules/documentos');
 require('./modules/fancybox-config');
 require('./modules/lazysizes-config');
-require('./modules/masonry-config');
 require('./modules/menu.js');
 require('./modules/picturefill-config');
