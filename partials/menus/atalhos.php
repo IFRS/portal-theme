f57e202b0dc927a9310371691a29e8d469b63fd3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-04 15:38:16 -0300

    Adaptação de classe extinta no Bootstrap 4

--- partials/menus/atalhos.php
+++ partials/menus/atalhos.php
@@ -1,6 +1,6 @@
-<ul class="hidden-xs menu-atalhos ">
-    <li><a href="#inicio-conteudo" accesskey="1"><?php _e('Ir para o conteúdo'); ?>&nbsp;<span class="badge">1</span></a></li>
-    <li><a href="#inicio-menu" accesskey="2"><?php _e('Ir para o menu'); ?>&nbsp;<span class="badge">2</span></a></li>
-    <li><a href="#search-field" accesskey="3"><?php _e('Ir para a busca'); ?>&nbsp;<span class="badge">3</span></a></li>
-    <li><a href="#inicio-rodape" accesskey="4"><?php _e('Ir para o rodapé'); ?>&nbsp;<span class="badge">4</span></a></li>
+<ul class="menu-atalhos ">
+    <li><a href="#inicio-conteudo" accesskey="1"><?php _e('Ir para o conteúdo'); ?>&nbsp;<span class="badge badge-secondary">1</span></a></li>
+    <li><a href="#inicio-menu" accesskey="2"><?php _e('Ir para o menu'); ?>&nbsp;<span class="badge badge-secondary">2</span></a></li>
+    <li><a href="#search-field" accesskey="3"><?php _e('Ir para a busca'); ?>&nbsp;<span class="badge badge-secondary">3</span></a></li>
+    <li><a href="#inicio-rodape" accesskey="4"><?php _e('Ir para o rodapé'); ?>&nbsp;<span class="badge badge-secondary">4</span></a></li>
 </ul>
