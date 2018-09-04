cc3fe5d9d2d9ee9726410eebad1e978d3ea6a5be
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-04 17:14:51 -0300

    Ajuste no botão do formulário de busca

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control searchform__field" placeholder="Buscar no portal..." required>
         <span class="input-group-append">
-            <button type="submit" class="btn btn-secondary searchform__submit" title="Buscar em todo o Portal" value="Buscar"><i class="fas fa-search" aria-hidden="true"></i></button>
+            <button type="submit" class="btn btn-outline-secondary searchform__submit" title="Buscar em todo o Portal" value="Buscar"><i class="fas fa-search" aria-hidden="true"></i></button>
         </span>
     </div>
 </form>
