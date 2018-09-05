b3702d42b8318be295e863d34192910a2cb2b318
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-05 14:22:31 -0300

    Ajuste no tamanho dos campos do formulário de busca

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" class="form-inline searchform" action="<?php echo esc_url(home_url('/')); ?>">
     <label class="sr-only" for="search-field">Buscar no portal por:</label>
     <div class="input-group">
-        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control searchform__field" placeholder="Buscar no portal..." required>
+        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control form-control-sm searchform__field" placeholder="Buscar no portal..." required>
         <span class="input-group-append">
-            <button type="submit" class="btn btn-outline-secondary searchform__submit" title="Buscar em todo o Portal" value="Buscar"><i class="fas fa-search" aria-hidden="true"></i></button>
+            <button type="submit" class="btn btn-outline-secondary btn-sm searchform__submit" title="Buscar em todo o Portal" value="Buscar"><i class="fas fa-search" aria-hidden="true"></i></button>
         </span>
     </div>
 </form>
