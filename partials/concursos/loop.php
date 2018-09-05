699f1a4b02a946f722d5c16180d8c9e94b5f154a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-05 11:50:38 -0300

    Adequação do componente panel para card do Bootstrap 4

--- partials/concursos/loop.php
+++ partials/concursos/loop.php
@@ -1,19 +1,19 @@
-<div class="panel-group" id="accordion-concursos" role="tablist" aria-multiselectable="true">
+<div class="accordion" id="accordion-concursos" role="tablist" aria-multiselectable="true">
 <?php while (have_posts()) : the_post(); ?>
-    <div class="panel panel-concurso">
-        <div class="panel-heading" role="tab" id="heading-<?php the_ID(); ?>">
+    <div class="card card-concurso">
+        <div class="card-header" role="tab" id="heading-<?php the_ID(); ?>">
         <?php $status = get_the_terms(get_the_ID(), 'concurso_status'); ?>
         <?php if ($status && is_post_type_archive('concurso')) : ?>
             <span class="label label-primary"><?php echo $status[0]->name; ?></span>
         <?php endif; ?>
-            <h3 class="panel-title">
-                <a class="accordion-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion-concursos" href="#collapse-<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse-<?php the_ID(); ?>">
+            <h3>
+                <a href="#collapse-<?php the_ID(); ?>" class="btn btn-link accordion-toggle" role="button" data-toggle="collapse" data-target="#collapse-<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse-<?php the_ID(); ?>">
                     <?php the_title(); ?>
                 </a>
             </h3>
         </div>
-        <div id="collapse-<?php the_ID(); ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-<?php the_ID(); ?>">
-            <div class="panel-body">
+        <div id="collapse-<?php the_ID(); ?>" class="collapse" role="tabpanel" aria-labelledby="heading-<?php the_ID(); ?>" data-parent="#accordion-concursos">
+            <div class="card-body">
                 <?php get_template_part('partials/concursos/item'); ?>
             </div>
         </div>
