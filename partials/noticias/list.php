866f302543eab6cd929df2032ffb99529c700001
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-11-06 15:13:23 -0200

    Separação da seção de título para facilitar a personalização em temas filhos

--- partials/noticias/list.php
+++ partials/noticias/list.php
@@ -2,19 +2,7 @@
     <div class="row">
         <div class="col-12">
             <h2 class="lista-noticias__title">
-            <?php
-                if (is_home()) {
-                    echo get_the_title(get_option( 'page_for_posts' ));
-                } elseif (is_category()) {
-                    echo single_cat_title(__('Not&iacute;cias da categoria&nbsp;'), false);
-                } elseif (is_tag()) {
-                    echo single_tag_title(__('Not&iacute;cias com a tag&nbsp;'), false);
-                } elseif (is_tax('escopo')) {
-                    echo single_term_title('Not&iacute;cias para ', false);
-                } else {
-                    echo 'Not&iacute;cias';
-                }
-            ?>
+                <?php get_template_part('partials/noticias/list-title'); ?>
             </h2>
         </div>
     </div>
