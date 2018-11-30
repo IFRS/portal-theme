d7dfc036f7a33acfaf4af5ac55ac01091a244c11
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-11-30 09:49:08 -0200

    Estilização da galeria com flexbox e adição de legendas

--- src/modules/fancybox-config.js
+++ src/modules/fancybox-config.js
@@ -2,4 +2,10 @@ require('jquery-fancybox/source/js/jquery.fancybox');
 
 $(function() {
     $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox();
+    $("a[rel='gallery']").each(function() {
+        var caption = $(this).parent().next('.gallery-caption').text();
+        if (caption) {
+            $(this).attr('title', $.trim(caption));
+        }
+    });
 });
