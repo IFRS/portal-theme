1001da990beac70d6b46871c1a21fa83f4babfab
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-02-06 15:07:48 -0200

    Ajuste na classe para Cursos na listagem para não conflitar com a página do Curso em si

--- partials/cursos/item.php
+++ partials/cursos/item.php
@@ -1,14 +1,14 @@
 <?php
     $niveis = get_the_terms(get_the_ID(), 'curso_nivel');
 ?>
-<div class="curso">
+<div class="curso-item">
     <?php foreach ($niveis as $nivel) : ?>
-        <span class="curso__nivel">
+        <span class="curso-item__nivel">
             <?php echo get_term_parents_list($nivel->term_id, 'curso_nivel', array('separator' => ' / ', 'inclusive' => false)); ?>
             <a href="<?php echo get_term_link($nivel); ?>"><?php echo $nivel->name; ?></a>
         </span>
     <?php endforeach; ?>
-    <h4 class="curso__title"><a href="<?php the_permalink(); ?>" class="curso__link"><?php the_title(); ?></a></h4>
-    <?php if (!is_tax('curso_modalidade')) : ?><span class="curso__modalidade"><?php the_terms( get_the_ID(), 'curso_modalidade', '', ', ' ); ?> | </span><?php endif; ?>
-    <span class="curso__turnos"><?php the_terms( get_the_ID(), 'curso_turno', '', ', ' ); ?></span>
+    <h4 class="curso-item__title"><a href="<?php the_permalink(); ?>" class="curso-item__link"><?php the_title(); ?></a></h4>
+    <?php if (!is_tax('curso_modalidade')) : ?><span class="curso-item__modalidade"><?php the_terms( get_the_ID(), 'curso_modalidade', '', ', ' ); ?> | </span><?php endif; ?>
+    <span class="curso-item__turnos"><?php the_terms( get_the_ID(), 'curso_turno', '', ', ' ); ?></span>
 </div>
