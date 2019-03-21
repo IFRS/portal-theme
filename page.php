46b84560d9e5c2facbc8f68c8cc64ddf6c4a639b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-03-21 17:42:02 -0300

    Adição da data de modificação nas páginas

--- page.php
+++ page.php
@@ -24,7 +24,10 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 col-sm-4">
+                    <p class="page__meta"><?php printf(__('Última atualização em %s', 'ifrs-portal-theme'), get_the_modified_date('d/m/Y')); ?></p>
+                </div>
+                <div class="col-12 col-sm-8">
                     <?php get_template_part('partials/share-buttons'); ?>
                 </div>
             </div>
