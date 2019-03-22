2cce7e57036b81a618e7e5abfbbe552def3ec40f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-03-22 15:05:04 -0300

    Adição de ícone para separar modalidade e turnos

--- partials/cursos/item.php
+++ partials/cursos/item.php
@@ -9,7 +9,7 @@
         </span>
     <?php endforeach; ?>
     <br>
-    <?php if (!is_tax('curso_modalidade')) : ?><span class="curso-item__modalidade"><?php the_terms( get_the_ID(), 'curso_modalidade', '', ', ' ); ?> | </span><?php endif; ?>
+    <?php if (!is_tax('curso_modalidade')) : ?><span class="curso-item__modalidade"><?php the_terms( get_the_ID(), 'curso_modalidade', '', ', ' ); ?> <i class="fas fa-grip-lines-vertical" aria-hidden="true"></i> </span><?php endif; ?>
     <span class="curso-item__turnos"><?php the_terms( get_the_ID(), 'curso_turno', '', ', ' ); ?></span>
     <h4 class="curso-item__title"><a href="<?php the_permalink(); ?>" class="curso-item__link"><?php the_title(); ?></a></h4>
 </div>
