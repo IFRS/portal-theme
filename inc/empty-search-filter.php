30e058f48aba5c0fd67cb9d405be09a61b2130bc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-07-04 16:34:42 -0300

    Ajuste para o WP não detectar busca com dados vazios

--- inc/empty-search-filter.php
+++ inc/empty-search-filter.php
@@ -1,7 +1,6 @@
 <?php
 function empty_search_filter($query) {
-   // If 's' request variable is set but empty
-   if (isset($_GET['s']) && empty($_GET['s']) && $query->is_main_query()) {
+   if (((isset($_GET['s']) && empty($_GET['s'])) || (isset($_POST['s']) && empty($_POST['s']))) && $query->is_main_query()) {
       $query->is_search = false;
    }
    return $query;
