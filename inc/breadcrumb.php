be12213170f187f5b6a8dbd0bb4d4213a93ecc23
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-07-05 14:24:16 -0300

    Correção no breadcrumb devido à nova forma de filtrar Cursos

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -31,29 +31,6 @@ function portal_breadcrumb() {
             echo $before . single_cat_title('', false) . $after;
         } elseif (is_search()) {
             echo $before . 'Resultado da pesquisa por: "' . get_search_query() . '"' . $after;
-        } elseif (is_tax('curso_modalidade')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('curso_nivel')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('curso_turno')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('concurso_status')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'concurso' ) . '">' . __('Concursos') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('documento_type') || is_tax('documento_origin')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'documento' ) . '">' . __('Documentos') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('edital_category')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'edital' ) . '">' . __('Editais') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('edital_status')) {
-            echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'edital' ) . '">' . __('Editais') . '</a></li>';
-            echo $before . single_term_title('', false) . $after;
-        } elseif (is_tax('escopo')) {
-            echo $before . single_term_title('Not&iacute;cias para ', false) . $after;
         } elseif (is_day()) {
             echo '<li class="breadcrumb-item"><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time(
                 'Y'
@@ -82,7 +59,39 @@ function portal_breadcrumb() {
                 echo $before . get_the_title() . $after;
             }
         } elseif (!is_single() && !is_page() && get_post_type() != 'post' && !is_404()) {
-            echo $before . post_type_archive_title('', false) . $after;
+            if (is_tax('curso_modalidade') && !isset($_POST['curso_modalidade'])) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
+                echo $before . 'Modalidades' . $after;
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('curso_unidade') && !isset($_POST['curso_unidade'])) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
+                echo $before . 'Unidades' . $after;
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('curso_nivel') && !isset($_POST['curso_nivel'])) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
+                echo $before . 'N&iacute;veis' . $after;
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('curso_turno') && !isset($_POST['curso_turno'])) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'curso' ) . '">' . __('Cursos') . '</a></li>';
+                echo $before . 'Turnos' . $after;
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('concurso_status')) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'concurso' ) . '">' . __('Concursos') . '</a></li>';
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('documento_type') || is_tax('documento_origin')) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'documento' ) . '">' . __('Documentos') . '</a></li>';
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('edital_category')) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'edital' ) . '">' . __('Editais') . '</a></li>';
+                echo $before . single_term_title('', false) . $after;
+            } elseif (is_tax('edital_status')) {
+                echo '<li class="breadcrumb-item"><a href="' . get_post_type_archive_link( 'edital' ) . '">' . __('Editais') . '</a></li>';
+                echo $before . single_term_title('', false) . $after;
+            } else {
+                echo $before . post_type_archive_title('', false) . $after;
+            }
+        } elseif (is_tax('escopo')) {
+            echo $before . single_term_title('Not&iacute;cias para ', false) . $after;
         } elseif (is_attachment()) {
             $parent = get_post($post->post_parent);
             $cat    = get_the_category($parent->ID);
@@ -92,7 +101,6 @@ function portal_breadcrumb() {
                 $parent
             ) . '">' . $parent->post_title . '</a></li> ';
             echo $before . get_the_title() . $after;
-
         } elseif (is_page() && !$post->post_parent) {
             echo $before . get_the_title() . $after;
         } elseif (is_page() && $post->post_parent) {
