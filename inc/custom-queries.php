6bca8906572226150f7045ebcf5cf64584e72af7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-11-22 09:30:06 -0300

    Remoção de templates e códigos relacionados ao plugin de Concursos

--- inc/custom-queries.php
+++ inc/custom-queries.php
@@ -1,12 +1,6 @@
 <?php
 function portal_custom_queries( $query ) {
     if (!is_admin() && $query->is_main_query()) {
-        if ($query->is_post_type_archive('concurso') || $query->is_tax('concurso_status')) {
-            $query->set('posts_per_page', -1);
-            $query->set('nopaging', true);
-            $query->set('orderby', 'modified');
-            $query->set('order', 'DESC');
-        }
         if ($query->is_post_type_archive('documento') || $query->is_tax('documento_type') || $query->is_tax('documento_origin')) {
             $query->set('posts_per_page', -1);
             $query->set('nopaging', true);
--- taxonomy-concurso_status.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<?php get_template_part('partials/concursos/concursos'); ?>
-
-<?php get_footer(); ?>
