58a2dd3be9eb46c1ebf559b32942781124f1b941
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-01-15 13:33:52 -0300

    Imagens destaque com proporção fixa de 4:3

--- partials/noticias/item.php
+++ partials/noticias/item.php
@@ -1,11 +1,13 @@
 <div class="row">
     <div class="col-12">
         <a href="<?php the_permalink(); ?>">
-        <?php if (has_post_thumbnail()) : ?>
-            <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
-        <?php else : ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/noticia-placeholder-<?php echo mt_rand(0, 9); ?>.png" alt="<?php the_title(); ?>" class="img-fluid noticia__img"/>
-        <?php endif; ?>
+            <div class="noticia__img-wrapper">
+                <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
+                <?php else : ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/noticia-placeholder-<?php echo mt_rand(0, 9); ?>.png" alt="<?php the_title(); ?>" class="img-fluid noticia__img"/>
+                <?php endif; ?>
+            </div>
         </a>
         <h2 class="noticia__titulo"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php
