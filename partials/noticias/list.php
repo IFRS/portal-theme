e5e880e9e8b209c286e2a5737a7ad7b325dbc93c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-01-23 10:20:32 -0300

    Ajuste na borda das notícias na listagem geral

--- partials/noticias/list.php
+++ partials/noticias/list.php
@@ -9,7 +9,7 @@
 
     <div class="card-deck lista-noticias__content">
     <?php while (have_posts()) : the_post(); ?>
-        <div class="card border-light">
+        <div class="card border-white">
             <div class="card-body p-0">
                 <article class="noticia">
                     <?php get_template_part('partials/noticias/item'); ?>
