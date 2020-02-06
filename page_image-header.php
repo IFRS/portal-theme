d5868d05dd8c1302e1389472c59e3c3bd1640444
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 11:52:07 -0300

    Limpeza de código desnecessário

--- page_image-header.php
+++ page_image-header.php
@@ -8,34 +8,30 @@
 
 <?php the_post(); ?>
 
-<div class="row">
-    <div class="col-12">
-        <article class="page">
-            <div class="row">
-                <div class="col-12">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <h2 class="sr-only"><?php the_title(); ?></h2>
-                        <?php the_post_thumbnail('full', array('class' => 'img-fluid mx-auto')); ?>
-                    <?php else : ?>
-                        <h2 class="page__title"><?php the_title(); ?></h2>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <?php get_template_part('partials/menus/subpages'); ?>
-            <div class="row">
-                <div class="col-12">
-                    <div class="page__content">
-                        <?php the_content(); ?>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <?php get_template_part('partials/share-buttons'); ?>
-                </div>
-            </div>
-        </article>
+<article class="page">
+    <div class="row">
+        <div class="col-12">
+            <?php if (has_post_thumbnail()) : ?>
+                <h2 class="sr-only"><?php the_title(); ?></h2>
+                <?php the_post_thumbnail('full', array('class' => 'img-fluid mx-auto')); ?>
+            <?php else : ?>
+                <h2 class="page__title"><?php the_title(); ?></h2>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
+    <?php get_template_part('partials/menus/subpages'); ?>
+    <div class="row">
+        <div class="col-12">
+            <div class="page__content">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <?php get_template_part('partials/share-buttons'); ?>
+        </div>
+    </div>
+</article>
 
 <?php get_footer(); ?>
--- page_index.php
+++ page_index.php
@@ -8,37 +8,33 @@
 
 <?php the_post(); ?>
 
-<div class="row">
-    <div class="col-12">
-        <article class="page">
-            <div class="row">
-                <div class="col-12">
-                    <h2 class="page__title"><?php the_title(); ?></h2>
-                </div>
-            </div>
-            <?php get_template_part('partials/menus/subpages-index'); ?>
-            <div class="row">
-                <div class="col-12">
-                    <div class="page__content">
-                        <?php
-                            if (has_post_thumbnail()) {
-                                the_post_thumbnail('full', array('class' => 'page__thumb'));
-                            }
-                        ?>
-                        <?php the_content(); ?>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 col-sm-4">
-                    <p class="page__meta"><?php printf(__('Última atualização em %s', 'ifrs-portal-theme'), get_the_modified_date('d/m/Y')); ?></p>
-                </div>
-                <div class="col-12 col-sm-8">
-                    <?php get_template_part('partials/share-buttons'); ?>
-                </div>
-            </div>
-        </article>
+<article class="page">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="page__title"><?php the_title(); ?></h2>
+        </div>
     </div>
-</div>
+    <?php get_template_part('partials/menus/subpages-index'); ?>
+    <div class="row">
+        <div class="col-12">
+            <div class="page__content">
+                <?php
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('full', array('class' => 'page__thumb'));
+                    }
+                ?>
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-sm-4">
+            <p class="page__meta"><?php printf(__('Última atualização em %s', 'ifrs-portal-theme'), get_the_modified_date('d/m/Y')); ?></p>
+        </div>
+        <div class="col-12 col-sm-8">
+            <?php get_template_part('partials/share-buttons'); ?>
+        </div>
+    </div>
+</article>
 
 <?php get_footer(); ?>
