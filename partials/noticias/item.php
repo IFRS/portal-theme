0e8175a5fb001e6273e71c13c14630cd8e91ea22
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 11:46:00 -0300

    Limpeza de código desnecessário

--- partials/noticias/item.php
+++ partials/noticias/item.php
@@ -1,31 +1,27 @@
-<div class="row">
-    <div class="col-12">
-        <a href="<?php the_permalink(); ?>">
-            <div class="noticia__img-wrapper">
-                <?php if (has_post_thumbnail()) : ?>
-                    <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
-                <?php else : ?>
-                    <img data-src="<?php echo get_template_directory_uri(); ?>/img/noticia-placeholder-<?php echo mt_rand(0, 9); ?>.png" alt="<?php the_title(); ?>" class="lazyload img-fluid noticia__img"/>
-                <?php endif; ?>
-            </div>
-        </a>
-        <h2 class="noticia__titulo"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php
-            $categories = get_the_category();
-            $cat_name = $categories[0]->cat_name;
-            $cat_ID = $categories[0]->term_id;
-        ?>
-        <p class="noticia__meta">
-        <?php if (!is_category()) : ?>
-            <span class="noticia__cartola">
-                <a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a>
-            </span>
-            -
+<a href="<?php the_permalink(); ?>">
+    <div class="noticia__img-wrapper">
+        <?php if (has_post_thumbnail()) : ?>
+            <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
+        <?php else : ?>
+            <img data-src="<?php echo get_template_directory_uri(); ?>/img/noticia-placeholder-<?php echo mt_rand(0, 9); ?>.png" alt="<?php the_title(); ?>" class="lazyload img-fluid noticia__img"/>
         <?php endif; ?>
-            <span class="noticia__data"><?php echo get_the_date(); ?></span>
-        </p>
-        <div class="noticia__resumo">
-            <?php the_excerpt(); ?>
-        </div>
     </div>
+</a>
+<h2 class="noticia__titulo"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php
+    $categories = get_the_category();
+    $cat_name = $categories[0]->cat_name;
+    $cat_ID = $categories[0]->term_id;
+?>
+<p class="noticia__meta">
+<?php if (!is_category()) : ?>
+    <span class="noticia__cartola">
+        <a href="<?php echo get_category_link($cat_ID); ?>"><?php echo $cat_name; ?></a>
+    </span>
+    -
+<?php endif; ?>
+    <span class="noticia__data"><?php echo get_the_date(); ?></span>
+</p>
+<div class="noticia__resumo">
+    <?php the_excerpt(); ?>
 </div>
--- partials/noticias/latest.php
+++ partials/noticias/latest.php
@@ -1,26 +1,20 @@
-<div class="row">
-    <div class="col-12">
-        <?php
-            global $post;
+<?php
+    $args = array(
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'post_type' => 'post',
+        'posts_per_page' => 10,
+        'post__not_in' => array($post->ID)
+    );
 
-            $args = array(
-                'orderby' => 'date',
-                'order' => 'DESC',
-                'post_type' => 'post',
-                'posts_per_page' => 10,
-                'post__not_in' => array($post->ID)
-            );
-
-            $last_posts = get_posts($args);
-        ?>
-        <?php if (!empty($last_posts)) : ?>
-            <div class="ultimos-posts">
-                <h2 class="ultimos-posts__title">&Uacute;ltimas Not&iacute;cias</h2>
-                <?php foreach ($last_posts as $post) : ?>
-                    <h3 class="ultimos-posts__post-title"><a href="<?php echo get_permalink($post->ID); ?>" rel="bookmark"><?php echo $post->post_title; ?></a></h3>
-                    <p class="ultimos-posts__post-date"><?php echo get_the_date('d/m/Y', $post->ID); ?></p>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
+    $last_posts = get_posts($args);
+?>
+<?php if (!empty($last_posts)) : ?>
+    <div class="ultimos-posts">
+        <h2 class="ultimos-posts__title">&Uacute;ltimas Not&iacute;cias</h2>
+        <?php foreach ($last_posts as $post) : ?>
+            <h3 class="ultimos-posts__post-title"><a href="<?php echo get_permalink($post->ID); ?>" rel="bookmark"><?php echo $post->post_title; ?></a></h3>
+            <p class="ultimos-posts__post-date"><?php echo get_the_date('d/m/Y', $post->ID); ?></p>
+        <?php endforeach; ?>
     </div>
-</div>
+<?php endif; ?>
