0891ccd57f5c0eda627cc8c2489477b60dae9b23
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 13:49:32 -0300

    Remoção de código que gerava incompatibilidade na galeria do WP

--- inc/custom-tinymce.php
+++ inc/custom-tinymce.php
@@ -8,7 +8,6 @@
 function portal_custom_tinymce($initArray) {
     $initArray['block_formats'] = 'Paragraph=p;Heading 1=h3;Heading 2=h4;Heading 3=h5;Heading 4=h6;Preformatted=pre;';
     $initArray['extended_valid_elements'] = 'iframe[id|class|title|style|frameborder|height|width|src|allowfullscreen]';
-    $initArray['invalid_elements'] = 'div';
 
     // paste as plain text
     $initArray['paste_remove_styles'] = true;
