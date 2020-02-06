2ccf5e1fb7a0678d4007382db6fb1ce38e7ce741
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 11:31:16 -0300

    Adição de lazy loading nativo

--- inc/lazyload.php
+++ inc/lazyload.php
@@ -28,6 +28,8 @@ function ifrs_add_lazyload($content) {
         $node->setAttribute('data-srcset', $oldsrcset);
         $newsrcset = '';
         $node->setAttribute('srcset', $newsrcset);
+
+        $node->setAttribute('loading', 'lazy');
     }
 
     $newHtml = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $dom->saveHTML()));
@@ -38,9 +40,11 @@ function ifrs_add_lazyload($content) {
 add_filter('the_content', 'ifrs_add_lazyload', 99);
 add_filter('post_thumbnail_html', 'ifrs_add_lazyload', 99);
 add_filter('get_avatar', 'ifrs_add_lazyload', 99);
+
 add_action( 'dynamic_sidebar_before', function() {
     ob_start();
 }, 0 );
+
 add_action( 'dynamic_sidebar_after', function() {
     $content = ob_get_clean();
 
