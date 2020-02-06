4b94e77011f1409279abd5f9a68af5afae9faf50
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 11:44:41 -0300

    Reestilização da Barra Brasil básica

--- partials/barrabrasil.php
+++ partials/barrabrasil.php
@@ -1,7 +1,7 @@
 <!-- Barra do Governo (http://barra.governoeletronico.gov.br/) -->
 <div id="barra-brasil" class="d-block">
-	<ul id="menu-barra-temp" class="bg-secondary list-inline mb-0 pl-5">
-		<li class="list-inline-item pb-1 pr-2 border-right"><a href="http://brasil.gov.br" class="text-white">Portal do Governo Brasileiro</a></li>
-		<li class="list-inline-item pb-1"><a href="http://epwg.governoeletronico.gov.br/barra/atualize.html" class="text-white">Atualize sua Barra de Governo</a></li>
+	<ul id="menu-barra-temp" class="bg-light list-inline mb-0 pl-5">
+		<li class="list-inline-item pb-1 pr-2 border-right"><a href="http://brasil.gov.br" class="text-reset">Portal do Governo Brasileiro</a></li>
+		<li class="list-inline-item pb-1"><a href="http://epwg.governoeletronico.gov.br/barra/atualize.html" class="text-reset">Atualize sua Barra de Governo</a></li>
 	</ul>
 </div>
