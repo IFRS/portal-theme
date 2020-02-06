b62d5e103b000cf90400064abb4c00ec67f9dd99
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-02-06 11:51:39 -0300

    Aperfeiçoamento da mensagem padrão para template não encontrado

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="alert alert-danger">
-    <p><strong><?php _e('Erro!'); ?></strong>&nbsp;<?php _e('Template n&atilde;o encontrado.'); ?></p>
+    <p><strong><?php _e('Erro!'); ?></strong>&nbsp;<?php _e('Template n&atilde;o encontrado.'); ?>&nbsp;Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
 </div>
 
 <?php get_footer(); ?>
