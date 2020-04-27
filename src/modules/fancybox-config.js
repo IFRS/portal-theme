806c2f7f78e0b6b65438f3eddf9d8ab0febd473b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-04-27 12:19:13 -0300

    Correção na galeria automática usando fancybox

--- src/modules/fancybox-config.js
+++ src/modules/fancybox-config.js
@@ -2,10 +2,12 @@ require('@fancyapps/fancybox');
 
 $(function() {
     $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']").attr('data-fancybox', 'gallery').each(function() {
-        $(this).fancybox();
-    });
-    $("a[data-fancybox='gallery']").each(function() {
-        var caption = $(this).parent().next('.gallery-caption').text();
+        let caption = $(this).parent().next('.gallery-caption').text();
+
+        if (!caption) {
+            caption = $(this).next('.wp-caption-text').text();
+        }
+
         if (caption) {
             $(this).attr('data-caption', $.trim(caption));
         }
