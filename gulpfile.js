05d54333496c79b3e579ae0218f16080dd897442
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-04-27 11:37:00 -0300

    Remoção do imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ const cssmin               = require('gulp-cssmin');
 const concat               = require('gulp-concat');
 const del                  = require('del');
 const gulp                 = require('gulp');
-const imagemin             = require('gulp-imagemin');
 const path                 = require('path');
 const pixrem               = require('pixrem');
 const PluginError          = require('plugin-error');
@@ -134,12 +133,6 @@ gulp.task('scripts', gulp.series('webpack', function js() {
     .pipe(browserSync.stream());
 }));
 
-gulp.task('images', function() {
-    return gulp.src('img/*.{png,jpg,gif}')
-    .pipe(imagemin())
-    .pipe(gulp.dest('img/'));
-});
-
 gulp.task('dist', function() {
     return gulp.src([
         '**',
@@ -158,7 +151,7 @@ gulp.task('dist', function() {
 });
 
 if (argv.production) {
-    gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts', 'images'), 'dist'));
+    gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts'), 'dist'));
 } else {
     gulp.task('build', gulp.series('clean', gulp.parallel('vendor-css', 'sass', 'webpack')));
 }
