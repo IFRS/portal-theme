69213f7870aa7dc3c125f7f83af08dda00465b6c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-06-22 15:22:40 -0300

    Ajuste no limite para profundidade das páginas

--- inc/depth-limit.php
+++ inc/depth-limit.php
@@ -1,23 +1,19 @@
 <?php
 /**
- * Limit max menu depth in admin panel to 2
+ * Limit max menu depth in admin panel to 3
  */
-function portal_menu_depth_limit( $hook ) {
+add_action( 'admin_enqueue_scripts', function( $hook ) {
     if ( $hook != 'nav-menus.php' ) return;
 
     // override default value right after 'nav-menu' JS
-    wp_add_inline_script( 'nav-menu', 'wpNavMenu.options.globalMaxDepth = 2;', 'after' );
-}
-
-add_action( 'admin_enqueue_scripts', 'portal_menu_depth_limit' );
+    wp_add_inline_script( 'nav-menu', 'wpNavMenu.options.globalMaxDepth = 3;', 'after' );
+} );
 
 // Thanks to certainlyakey
 // https://gist.github.com/certainlyakey/9da9bab22cd073ee486d
-// Limit pages nesting depth to 2nd level
+// Limit pages nesting depth to 3nd level
 // caution! by default affects all hierarchical CPTs
-function portal_limit_pages_nesting($a) {
-	$a['depth'] = 2;
+add_action( 'page_attributes_dropdown_pages_args', function( $a ) {
+	$a['depth'] = 3;
 	return $a;
-}
-
-add_action('page_attributes_dropdown_pages_args','portal_limit_pages_nesting');
+} );
