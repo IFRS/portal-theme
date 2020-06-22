af3c9b50dfae57affeacc69b3ac8c4d80e663439
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-06-22 16:15:12 -0300

    Ajuste para evitar dar update no banco a todo momento

--- inc/disable-medium_large.php
+++ inc/disable-medium_large.php
@@ -1,3 +1,5 @@
 <?php
-update_option('medium_large_size_w', '0');
-update_option('medium_large_size_h', '0');
+add_action('after_switch_theme', function() {
+    update_option('medium_large_size_w', '0');
+    update_option('medium_large_size_h', '0');
+});
