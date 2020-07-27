d12e0cc90475c6cfec678425a21b2411c35726a2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-07-27 16:52:59 -0300

    Correção no nome do arquivo

--- inc/fonts.php
+++ inc/fonts.php
@@ -1,6 +1,6 @@
 <?php
 add_action('wp_head', function() {
-    $fonts_file = get_template_directory_uri().(WP_DEBUG ? '/css/fonts.css' : '/css/fonts.min.css');
+    $fonts_file = get_template_directory_uri(). '/css/fonts.css';
 ?>
     <link rel="preload" as="style" href="<?php echo $fonts_file; ?>"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
