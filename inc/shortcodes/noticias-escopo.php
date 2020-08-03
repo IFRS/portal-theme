67c8005abc3ba2ec5a02b2f3b1ae2aa27a54e954
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-03 15:39:35 -0300

    Ajustes na lista de notícias por escopo

--- inc/shortcodes/noticias-escopo.php
+++ inc/shortcodes/noticias-escopo.php
@@ -19,27 +19,25 @@ function noticias_escopo_shortcode($atts, $escopo = '') {
 
     ob_start();
 ?>
-        <div class="row">
-            <div class="col-12">
-                <h2 class="title-box">Not&iacute;cias para <?php echo $escopo->name; ?></h2>
-            </div>
-        </div>
-        <div class="row" id="lista-noticias">
+    <div class="lista-noticias">
+        <h2 class="lista-noticias__title">Not&iacute;cias para <?php echo $escopo->name; ?></h2>
+        <div class="card-deck lista-noticias__content">
         <?php while ($noticias->have_posts()) : $noticias->the_post(); ?>
-            <div class="col-12 col-sm-6 col-md-3">
-                <article class="noticia">
-                    <?php get_template_part('partials/noticias/item'); ?>
-                </article>
-            </div>
-        <?php endwhile; ?>
-        <?php wp_reset_query(); ?>
-            <div class="col-12">
-                <div class="acesso-todas-noticias">
-                    <hr class="acesso-todas-noticias__separador">
-                    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="float-right acesso-todas-noticias__link"><?php _e('Acesse mais notícias'); ?></a>
+            <div class="card border-white">
+                <div class="card-body p-0">
+                    <article class="noticia">
+                        <?php get_template_part('partials/noticias/item'); ?>
+                    </article>
                 </div>
             </div>
+        <?php endwhile; ?>
         </div>
+        <?php wp_reset_query(); ?>
+        <div class="acesso-todas-noticias">
+            <hr class="acesso-todas-noticias__separador">
+            <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="float-right acesso-todas-noticias__link"><?php _e('Acesse mais notícias'); ?></a>
+        </div>
+    </div>
 <?php
     return ob_get_clean();
 }
