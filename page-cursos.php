5bec395169989c9414c1533cefb5a22d45a80898
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-08-07 18:09:45 -0300

    Criação de template exclusivo para página de Cursos

---
+++ page-cursos.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+<article class="ursos">
+    <h2 class="cursos__title"><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+</article>
+
+<?php get_footer(); ?>
