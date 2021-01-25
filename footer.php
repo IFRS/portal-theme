4f8ae5fc0394030188a1bbde09691ad7965d252f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-01-25 16:32:19 -0300

    Ajustes no posicionamento dos créditos

--- footer.php
+++ footer.php
@@ -33,15 +33,15 @@
                     <a href="https://www.gov.br/acessoainformacao" class="d-block px-sm-5 px-md-0"><img data-src="<?php echo get_template_directory_uri(); ?>/img/acesso-info.svg" alt="Acesso &agrave; Informa&ccedil;&atilde;o" class="lazyload mx-auto"></a>
                     <div class="creditos">
                         <!-- Wordpress -->
-                        <a href="https://br.wordpress.org/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="right" title="Desenvolvido com Wordpress">
+                        <a href="https://br.wordpress.org/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="top" title="Desenvolvido com Wordpress">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-wordpress.png" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                         <!-- Código-fonte -->
-                        <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="right" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
+                        <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="top" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-git.png" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                         <!-- Creative Commons -->
-                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener noreferrer license" data-toggle="tooltip" data-placement="right" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
+                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener noreferrer license" data-toggle="tooltip" data-placement="top" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-cc-by-nc-sa.png" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                     </div>
