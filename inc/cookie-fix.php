4ca7f5e0df583bacf36944a5859dff8d8c9c7940
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-01-25 13:22:18 -0300

    Atualização das opções de geração de cookies

--- inc/cookie-fix.php
+++ inc/cookie-fix.php
@@ -1,5 +1,19 @@
 <?php
-setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN);
+setcookie(TEST_COOKIE, 'WP Cookie check', array (
+    'expires' => 0,
+    'path' => COOKIEPATH,
+    'domain' => COOKIE_DOMAIN,
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+));
 if ( SITECOOKIEPATH != COOKIEPATH ) {
-    setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
+    setcookie(TEST_COOKIE, 'WP Cookie check', array (
+        'expires' => 0,
+        'path' => SITECOOKIEPATH,
+        'domain' => COOKIE_DOMAIN,
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ));
 }
