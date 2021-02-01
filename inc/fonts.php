8be70b4aeed1aa87671118d2e95b3a8d5eaac4f9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-02-01 14:25:46 -0300

    Preload de arquivos de fonte

--- inc/fonts.php
+++ inc/fonts.php
@@ -2,7 +2,9 @@
 add_action('wp_head', function() {
     $fonts_file = get_template_directory_uri(). '/css/fonts.css';
 ?>
-    <link rel="preload" as="style" href="<?php echo $fonts_file; ?>"/>
+    <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/opensans/Regular/OpenSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/opensans/Bold/OpenSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
         <link rel="stylesheet" href="<?php echo $fonts_file; ?>"/>
