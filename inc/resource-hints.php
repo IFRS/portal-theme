6ccfc30d1d54381dadd1c4f9e4b7138bb40e8786
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-02-01 14:25:13 -0300

    Correção de identação

--- inc/resource-hints.php
+++ inc/resource-hints.php
@@ -2,8 +2,8 @@
 if (!WP_DEBUG) {
     add_action('wp_head', function() {
 ?>
-        <link rel="preconnect" href="https://vlibras.gov.br">
-        <link rel="preconnect" href="https://barra.brasil.gov.br">
+    <link rel="preconnect" href="https://vlibras.gov.br">
+    <link rel="preconnect" href="https://barra.brasil.gov.br">
 <?php
     }, 0);
 }
