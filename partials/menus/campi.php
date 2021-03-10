9762e323c6733415354255283909d934a39639ec
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-10 14:03:23 -0300

    Reestilização do menu de campi

--- partials/menus/campi.php
+++ partials/menus/campi.php
@@ -1,13 +1,15 @@
 <?php
     wp_nav_menu(
         array(
-            'menu_class'        => 'menu-campi',
-            'menu_id'           => false,
-            'container'         => 'nav',
-            'container_class'   => false,
-            'container_id'      => false,
-            'depth'             => 1,
-            'theme_location'    => 'campi',
+            'menu_class'           => 'menu-campi',
+            'menu_id'              => false,
+            'container'            => 'nav',
+            'container_class'      => false,
+            'container_id'         => false,
+            'container_aria_label' => 'Lista de Campi',
+            'depth'                => 1,
+            'theme_location'       => 'campi',
+            'item_spacing'         => 'discard',
         )
     );
 ?>
