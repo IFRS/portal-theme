6ebe5d447fe5e609047f66ffa713b224ab874be6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-17 11:08:56 -0300

    Ajustes nos links da área de créditos

--- footer.php
+++ footer.php
@@ -30,18 +30,20 @@
                     <?php if (!dynamic_sidebar('widget-footer')) : endif; ?>
                 </div>
                 <div class="col-12 col-md-5 offset-md-1 col-lg-3 offset-lg-3">
-                    <a href="https://www.gov.br/acessoainformacao" class="footer__lai d-block px-sm-5 px-md-0" rel="noopener noreferrer" data-toggle="tooltip" data-placement="top" title="Site governamental sobre a Lei de Acesso à Informação (LAI)"><img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/acesso-info.svg" alt="Acesso &agrave; Informa&ccedil;&atilde;o" class="lazyload mx-auto"></a>
+                    <a href="https://www.gov.br/acessoainformacao" class="footer__lai d-block px-sm-5 px-md-0" rel="noopener" data-toggle="tooltip" data-placement="top" title="Site governamental sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o (LAI)">
+                        <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/acesso-info.svg" alt="Site governamental sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o (abre uma nova p&aacute;gina)" class="lazyload mx-auto">
+                    </a>
                     <div class="creditos">
                         <!-- Wordpress -->
-                        <a href="https://br.wordpress.org/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="top" title="Desenvolvido com Wordpress">
+                        <a href="https://br.wordpress.org/" target="_blank" rel="noopener" data-toggle="tooltip" data-placement="top" title="Desenvolvido com Wordpress">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-wordpress.png" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                         <!-- Código-fonte -->
-                        <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="top" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
+                        <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener" data-toggle="tooltip" data-placement="top" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-git.png" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                         <!-- Creative Commons -->
-                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener noreferrer license" data-toggle="tooltip" data-placement="top" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
+                        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener license" data-toggle="tooltip" data-placement="top" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
                             <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/creditos-cc-by-nc-sa.png" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)" class="lazyload"/>
                         </a>
                     </div>
