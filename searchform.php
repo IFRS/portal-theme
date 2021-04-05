128aa13a54d1179b9697e29aed40c6daf07bbbdb
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-05 17:27:05 -0300

    Alteração nas cores do botão e label do formulário de busca

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control form-control-sm border-secondary searchform__field" placeholder="Buscar no portal" required>
         <label class="searchform__label" for="search-field">Buscar no portal</label>
         <span class="input-group-append">
-            <button type="submit" class="btn btn-secondary btn-sm searchform__submit" title="Buscar em todo o Portal" value="Buscar"></button>
+            <button type="submit" class="btn btn-dark btn-sm searchform__submit" title="Buscar em todo o Portal" value="Buscar"></button>
         </span>
     </div>
 </form>
