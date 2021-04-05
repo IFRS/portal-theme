258b869d93751ee368537e70b9f62222547513fc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-04-05 15:36:42 -0300

    Adição de float label no formulário de busca

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="form-inline searchform" action="<?php echo esc_url(home_url('/')); ?>">
-    <label class="sr-only" for="search-field">Buscar no portal por:</label>
     <div class="input-group">
-        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control form-control-sm border-secondary searchform__field" placeholder="Buscar no portal..." required>
+        <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="form-control form-control-sm border-secondary searchform__field" placeholder="Buscar no portal" required>
+        <label class="searchform__label" for="search-field">Buscar no portal</label>
         <span class="input-group-append">
             <button type="submit" class="btn btn-secondary btn-sm searchform__submit" title="Buscar em todo o Portal" value="Buscar"></button>
         </span>
