bb6fcbc0c48e960ddd837d34f8d422f9950ba10a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-12-22 11:27:28 -0300

    Pré-carregamento da marca principal

--- inc/resource-hints.php
+++ inc/resource-hints.php
@@ -1,9 +1,12 @@
 <?php
-if (!WP_DEBUG) {
-    add_action('wp_head', function() {
+add_action('wp_head', function() {
 ?>
     <link rel="preconnect" href="https://vlibras.gov.br">
     <link rel="preconnect" href="https://barra.brasil.gov.br">
 <?php
-    }, 0);
-}
+    if (has_custom_logo()) {
+        echo '<link rel="preload" href="' . esc_url(wp_get_attachment_image_src(get_theme_mod('custom_logo'), 'full')[0]) . '" as="image" crossorigin="anonymous"/>';
+    } else {
+        echo '<link rel="preload" href="' . get_stylesheet_directory_uri() . '/img/ifrs.png" as="image" crossorigin="anonymous"/>';
+    }
+}, 0);
