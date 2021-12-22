ece2784522f2eab8523e4d776eddcd24e4ebc920
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-12-22 13:07:02 -0300

    Mudança na proporção forçada das imagens das notícias

--- partials/noticias/item.php
+++ partials/noticias/item.php
@@ -1,11 +1,9 @@
 <a href="<?php the_permalink(); ?>" class="noticia__link">
-    <div class="noticia__img-wrapper">
-        <?php if (has_post_thumbnail()) : ?>
-            <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
-        <?php else : ?>
-            <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/noticia-placeholder.jpg" alt="<?php the_title(); ?>" class="lazyload img-fluid noticia__img" loading="lazy" width="720" height="540" aria-hidden="true"/>
-        <?php endif; ?>
-    </div>
+    <?php if (has_post_thumbnail()) : ?>
+        <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid noticia__img')); ?>
+    <?php else : ?>
+        <img data-src="<?php echo esc_url(get_template_directory_uri()); ?>/img/noticia-placeholder.jpg" alt="" class="lazyload img-fluid noticia__img" loading="lazy" width="720" height="540" aria-hidden="true"/>
+    <?php endif; ?>
     <h2 class="noticia__titulo"><?php the_title(); ?></h2>
 </a>
 <?php
