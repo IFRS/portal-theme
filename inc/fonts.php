75f20ea9310f38fb33fddfc7587cea68bad02f2e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-12-22 10:51:46 -0300

    Remoção de pré-carregamento de fonte em negrito

--- inc/fonts.php
+++ inc/fonts.php
@@ -3,7 +3,6 @@ add_action('wp_head', function() {
     $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
 ?>
     <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/opensans/Regular/OpenSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/opensans/Bold/OpenSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
     <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
