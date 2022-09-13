8bf0232ed93a5e9351840e781b342202f54e6166
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-09-13 16:56:27 -0300

    Ajuste na adição de imagem destacada no feed RSS

--- inc/feed-thumbnail.php
+++ inc/feed-thumbnail.php
@@ -1,13 +1,18 @@
 <?php
-function ifrs_portal_rss_post_thumbnail($content) {
-    global $post;
+add_action( 'rss2_item', function() {
+	if (!has_post_thumbnail(get_the_ID())) return;
 
-    if (has_post_thumbnail($post->ID)) {
-        $content = '<p>' . get_the_post_thumbnail($post->ID) . '</p>' . get_the_content();
-    }
+	$thumbnail_id = get_post_thumbnail_id(get_the_ID());
+    $thumbnail_file = get_attached_file($thumbnail_id);
 
-    return $content;
-}
+	if (empty($thumbnail_file)) return;
 
-add_filter('the_excerpt_rss', 'ifrs_portal_rss_post_thumbnail');
-add_filter('the_content_feed', 'ifrs_portal_rss_post_thumbnail');
+	$upload_dir = wp_upload_dir();
+
+	printf(
+		'<enclosure url="%s" length="%s" type="%s" />',
+		get_the_post_thumbnail_url(get_the_ID()),
+		filesize($thumbnail_file),
+		get_post_mime_type($thumbnail_id)
+	);
+} );
