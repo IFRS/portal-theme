81ec14c771ac387235d0439487d636a942d02338
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-11-29 17:13:14 -0300

    Criação do template para o banner do e-MEC

---
+++ partials/banner-e-mec.php
@@ -0,0 +1,14 @@
+<?php
+  $banner_enabled = cmb2_get_option( 'e_mec_options', 'e_mec_enable' );
+  $banner_img = wp_get_attachment_image( cmb2_get_option( 'e_mec_options', 'e_mec_img_id' ), 'full', false, array( 'class' => 'd-block lazyload img-fluid mx-auto' ) );
+  $banner_url = cmb2_get_option( 'e_mec_options', 'e_mec_url' );
+?>
+<?php if (!empty($banner_enabled) && !empty($banner_img)) : ?>
+  <?php if (!empty($banner_url)) : ?>
+    <a href="<?php echo $banner_url ?>" target="_blank">
+  <?php endif; ?>
+    <?php echo $banner_img; ?>
+  <?php if (!empty($banner_url)) : ?>
+    </a>
+  <?php endif; ?>
+<?php endif; ?>
