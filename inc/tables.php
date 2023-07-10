e8c7292abcac62f22259bb3d40de84bff3e17106
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-07-10 13:13:45 -0300

    Correção no uso do DOMDocument caso conteúdo esteja vazio

--- inc/tables.php
+++ inc/tables.php
@@ -1,24 +1,29 @@
 <?php
 add_filter( 'the_content', function($content) {
     $content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
-    $dom = new DOMDocument();
-    @$dom->loadHTML($content);
 
-    $tables = [];
+    if ($content) {
+        $dom = new DOMDocument();
+        @$dom->loadHTML($content);
 
-    foreach ($dom->getElementsByTagName('table') as $node) {
-        $tables[] = $node;
+        $tables = [];
+
+        foreach ($dom->getElementsByTagName('table') as $node) {
+            $tables[] = $node;
+        }
+
+        foreach ($tables as $node) {
+            $fallback = $node->cloneNode(true);
+
+            $oldClass = $node->getAttribute('class');
+            $newClass = 'table ' . $oldClass;
+            $node->setAttribute('class', $newClass);
+        }
+
+        $newHtml = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $dom->saveHTML()));
+
+        return $newHtml;
+    } else {
+        return $content;
     }
-
-    foreach ($tables as $node) {
-        $fallback = $node->cloneNode(true);
-
-        $oldClass = $node->getAttribute('class');
-        $newClass = 'table ' . $oldClass;
-        $node->setAttribute('class', $newClass);
-    }
-
-    $newHtml = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $dom->saveHTML()));
-
-    return $newHtml;
 } );
