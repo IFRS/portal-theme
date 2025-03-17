99f80eadb0a188d70572ec48545353ccc29bf016
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-17 11:55:11 -0300

    Uso de função padrão do WP para incluir arquivos

--- functions.php
+++ functions.php
@@ -1,95 +1,95 @@
 <?php
 // Restrições para usuários
-require_once('inc/restrictions.php');
+require_once get_parent_theme_file_path('inc/restrictions.php');
 
 // Cookie Fix
-require_once('inc/cookie-fix.php');
+require_once get_parent_theme_file_path('inc/cookie-fix.php');
 
 // Suporte para diversas funções
-require_once('inc/theme-support.php');
+require_once get_parent_theme_file_path('inc/theme-support.php');
 
 // Remoção de thumbnails medium_large
-require_once('inc/disable-medium_large.php');
+require_once get_parent_theme_file_path('inc/disable-medium_large.php');
 
 // Thumbnails no feed RSS
-require_once('inc/feed-thumbnail.php');
+require_once get_parent_theme_file_path('inc/feed-thumbnail.php');
 
 // Títulos personalizados
-require_once('inc/custom-title.php');
+require_once get_parent_theme_file_path('inc/custom-title.php');
 
 // Limita o número de níveis na construção dos menus e de aninhamento das páginas
-require_once('inc/depth-limit.php');
+require_once get_parent_theme_file_path('inc/depth-limit.php');
 
 // Desabilita a geração de links para páginas de anexo
-require_once('inc/disable-attachment-link.php');
+require_once get_parent_theme_file_path('inc/disable-attachment-link.php');
 
 // Preconnect, Prefetch, Preload, etc...
-require_once('inc/resource-hints.php');
+require_once get_parent_theme_file_path('inc/resource-hints.php');
 
 // Fonts
-require_once('inc/fonts.php');
+require_once get_parent_theme_file_path('inc/fonts.php');
 
 // Scripts & Styles
-require_once('inc/assets.php');
+require_once get_parent_theme_file_path('inc/assets.php');
 
 // Registra os menus
-require_once('inc/menus.php');
+require_once get_parent_theme_file_path('inc/menus.php');
 
 // Breadcrumb
-require_once('inc/breadcrumb.php');
+require_once get_parent_theme_file_path('inc/breadcrumb.php');
 
 // Paginação personalizada
-require_once('inc/pagination.php');
+require_once get_parent_theme_file_path('inc/pagination.php');
 
 // Custom Queries
-require_once('inc/custom-queries.php');
+require_once get_parent_theme_file_path('inc/custom-queries.php');
 
 // Share Buttons
-require_once('inc/share.php');
+require_once get_parent_theme_file_path('inc/share.php');
 
 // Filtro para buscas vazias
-require_once('inc/empty-search-filter.php');
+require_once get_parent_theme_file_path('inc/empty-search-filter.php');
 
 // Widgets
-require_once('inc/widgets.php');
+require_once get_parent_theme_file_path('inc/widgets.php');
 
 // Custom TinyMCE
-require_once('inc/custom-tinymce.php');
+require_once get_parent_theme_file_path('inc/custom-tinymce.php');
 
 // Permitir tag iframe
-require_once('inc/allow-iframe.php');
+require_once get_parent_theme_file_path('inc/allow-iframe.php');
 
 // Vídeos do YouTube responsivos
-require_once('inc/responsive-youtube-embed.php');
+require_once get_parent_theme_file_path('inc/responsive-youtube-embed.php');
 
 // Customização do resumo dos posts
-require_once('inc/excerpt.php');
+require_once get_parent_theme_file_path('inc/excerpt.php');
 
 // Configurações da Galeria
-require_once('inc/gallery.php');
+require_once get_parent_theme_file_path('inc/gallery.php');
 
 // LazyLoad
-require_once('inc/lazyload.php');
+require_once get_parent_theme_file_path('inc/lazyload.php');
 
 // Tables
-require_once('inc/tables.php');
+require_once get_parent_theme_file_path('inc/tables.php');
 
 // Disable emoji
-require_once('inc/disable-emoji.php');
+require_once get_parent_theme_file_path('inc/disable-emoji.php');
 
 // Remove all "version" text from output
-require_once('inc/remove-version.php');
+require_once get_parent_theme_file_path('inc/remove-version.php');
 
 // Configurações Personalizadas
-require_once('inc/options.php');
+require_once get_parent_theme_file_path('inc/options.php');
 
 // Metaboxes
-require_once('inc/pages-metaboxes.php');
+require_once get_parent_theme_file_path('inc/pages-metaboxes.php');
 
 // Taxonomias
-require_once('inc/taxonomies/escopo.php');
+require_once get_parent_theme_file_path('inc/taxonomies/escopo.php');
 
 // Shortcodes
-require_once('inc/shortcodes/bootstrap4.php');
-require_once('inc/shortcodes/noticias-escopo.php');
-require_once('inc/shortcodes/posts-by-category.php');
+require_once get_parent_theme_file_path('inc/shortcodes/bootstrap4.php');
+require_once get_parent_theme_file_path('inc/shortcodes/noticias-escopo.php');
+require_once get_parent_theme_file_path('inc/shortcodes/posts-by-category.php');
