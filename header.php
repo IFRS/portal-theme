6f0c8682017f0058f9df9f047a2e665ef108b228
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-18 14:54:02 -0300

    Ajustes na marca gov.br com adição de link e mudança de tamanho

--- header.php
+++ header.php
@@ -27,10 +27,12 @@
       <?php get_template_part('partials/menus/atalhos'); ?>
 
       <div class="row align-items-center">
-        <div class="col-12 col-lg-4 d-flex align-items-center">
-          <img src="<?php echo get_theme_file_uri( '/img/govbr.png' ) ?>" class="header__govbr" alt="Governo do Brasil" width="200" height="72">
+        <div class="col-12 col-lg-4 d-flex align-items-start">
+          <a href="https://gov.br/">
+            <img src="<?php echo get_theme_file_uri( '/img/govbr.png' ) ?>" class="header__govbr" alt="Governo do Brasil" width="200" height="72">
+          </a>
           <div class="vr mx-3"></div>
-          <p class="m-0 fs-5">Ministério da Educação</p>
+          <p class="m-0 fs-5">Minist&eacute;rio da Educa&ccedil;&atilde;o</p>
         </div>
         <div class="col-12 col-lg-8">
           <?php get_template_part('partials/menus/acessibilidade'); ?>
