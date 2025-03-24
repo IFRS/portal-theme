975c0ff46a40599e0eaa0436b6eba354a84ea087
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-24 10:51:48 -0300

    Ajustes no padrão para notícias

--- inc/block-patterns/noticias.php
+++ inc/block-patterns/noticias.php
@@ -5,15 +5,15 @@ register_block_pattern('core/query-noticias', array(
 	'blockTypes' => array( 'core/query' ),
 	'categories' => array( 'query' ),
 	'content'    => '
-    <!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"include","inherit":false},"displayLayout":{"type":"flex","columns":3}} -->
+    <!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"disable_pagination":true},"displayLayout":{"type":"flex","columns":3}} -->
     <div class="wp-block-query">
-    <!-- wp:post-template -->
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"layout":{"inherit":false}} -->
+    <!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+    <!-- wp:group {"layout":{"inherit":false}} -->
     <div class="wp-block-group">
     <!-- wp:post-featured-image {"isLink":true} /-->
     <!-- wp:post-terms {"term":"category"} /-->
     <!-- wp:post-title {"isLink":true} /-->
-    <!-- wp:post-excerpt /-->
+    <!-- wp:post-excerpt {"excerptLength":30} /-->
     <!-- wp:post-date /-->
     </div>
     <!-- /wp:group -->
