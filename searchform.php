86cb02e8030a81e80a04cc73c1ee225a8540f043
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-26 11:11:07 -0300

    Ajuste no formulário de busca geral

--- searchform.php
+++ searchform.php
@@ -1,11 +1,8 @@
 <form role="search" method="get" class="form-inline searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <div class="input-group">
-    <label class="visually-hidden" for="portal-campo-busca">Buscar em todo Portal</label>
-    <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="portal-campo-busca" class="form-control" placeholder="Digite um termo para a busca" required>
-    <span class="input-group-append">
-      <button type="submit" class="btn btn-light" value="Buscar">
-        <i class="fa-solid fa-magnifying-glass"></i>
-      </button>
-    </span>
+    <input type="search" value="<?php echo get_search_query(); ?>" name="s" required id="portal-campo-busca" class="form-control" placeholder="Digite um termo para a busca" aria-label="Buscar em todo o portal">
+    <button type="submit" class="btn btn-outline-secondary" value="Buscar">
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </button>
   </div>
 </form>
