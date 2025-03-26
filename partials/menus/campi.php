5bb6fabdfc5ca626168138cb0892c93422225a21
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-26 11:49:25 -0300

    Ajustes na navbar e nos cantos arredondados padrão

--- partials/menus/campi.php
+++ partials/menus/campi.php
@@ -1,7 +1,7 @@
 <?php $id = uniqid('nav-campi-') ?>
 <nav class="menu-campi navbar navbar-expand-md bg-body-tertiary mt-3">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $id; ?>" aria-expanded="false" aria-controls="<?php echo $id; ?>" aria-label="Alterna lista de Campi">
+    <button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $id; ?>" aria-expanded="false" aria-controls="<?php echo $id; ?>" aria-label="Alterna lista de Campi">
       <span class="navbar-toggler-icon"></span>&nbsp;<?php _e('Campi do IFRS', 'ifrs-portal-theme'); ?>
     </button>
     <?php
@@ -11,7 +11,7 @@
           'container_class'      => 'collapse navbar-collapse',
           'container_id'         => $id,
           'container_aria_label' => 'Lista de Campi',
-          'menu_class'           => 'navbar-nav flex-wrap my-3 mx-auto',
+          'menu_class'           => 'navbar-nav flex-wrap mx-auto',
           'menu_id'              => false,
           'depth'                => 1,
           'theme_location'       => 'campi',
