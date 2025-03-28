1da4cbab004c9961819d85cd22f6df5d55d4e72e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-28 15:13:43 -0300

    Criação de pattern para link automático da página inicial

---
+++ patterns/link-front-page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Link para a página inicial
+ * Slug: ifrs/link-front-page
+ * Categories: link
+ * Block Types: core/paragraph
+ */
+?>
+<a href="<?php echo esc_url( home_url() ); ?>" data-type="page">p&aacute;gina inicial</a>
