54806ab415f16d00879a4d061c42af26669d9c27
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-28 15:06:45 -0300

    Refatoração do template para erro 404

---
+++ patterns/image-404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Imagem para erro 404
+ * Slug: ifrs/image-404
+ * Categories: image
+ * Block Types: core/template/404
+ */
+?>
+
+<!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","className":"img-fluid"} -->
+<figure class="wp-block-image aligncenter size-full img-fluid"><img src="<?php echo get_parent_theme_file_uri( 'img/404.png' ); ?>" alt="Erro 404 - Página não encontrada"/></figure>
+<!-- /wp:image -->
