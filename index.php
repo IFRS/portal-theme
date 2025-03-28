b98012687184d9a0bc8608ebeed7b78d4da8cb2b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-28 15:06:54 -0300

    Remoção de código legado

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php get_header(); ?>
-
-<div class="alert alert-danger">
-    <p><strong><?php _e('Erro!', 'ifrs-portal-theme'); ?></strong>&nbsp;<?php _e('Template n&atilde;o encontrado.', 'ifrs-portal-theme'); ?>&nbsp;Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
-</div>
-
-<?php get_footer(); ?>
+<?php
+// Silence is golden
+// This file is intentionally left blank.
