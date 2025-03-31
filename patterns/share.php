6b8c306f228b0d8470488cacf9cb43f9b02366de
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-03-31 16:59:27 -0300

    Ajuste nos botões de compartilhamento, adição de e-mail

--- patterns/share.php
+++ patterns/share.php
@@ -12,21 +12,22 @@ $title = urlencode(get_the_title());
 
 $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );
 
-$facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$URL;
-$twitterURL = 'https://twitter.com/intent/tweet?text='.$title.'&amp;url='.$URL;
-$linkedinURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$URL.'&amp;title='.$title;
-$whatsappURL = 'whatsapp://send?text='.$title.' '.$URL;
-
-ob_start();
+$emailURL = "mailto:?subject=$title&body=$URL";
+$facebookURL = "https://www.facebook.com/sharer.php?u=$URL";
+$twitterURL = "https://twitter.com/intent/tweet?text=$title&amp;url=$URL";
+$linkedinURL = "https://www.linkedin.com/sharing/share-offsite/?url=$URL";
+$whatsappURL = "whatsapp://send/?text=$title%20$URL";
 ?>
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
-<div class="wp-block-group">
+<div class="wp-block-group" style="color: var(--bs-secondary);">
   <!-- wp:html -->
     <i class="fa-solid fa-share-nodes"></i>
     <span class="visually-hidden">Compartilhar conte&uacute;do:</span>
   <!-- /wp:html -->
-  <!-- wp:social-links {"size":"has-small-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
-  <ul class="wp-block-social-links has-small-icon-size is-style-logos-only">
+  <!-- wp:social-links {"className":"is-style-logos-only","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
+  <ul class="wp-block-social-links is-style-logos-only">
+    <!-- wp:social-link {"url":"<?php echo $emailURL; ?>","service":"mail"} /-->
+
     <!-- wp:social-link {"url":"<?php echo $facebookURL; ?>","service":"facebook"} /-->
 
     <!-- wp:social-link {"url":"<?php echo $twitterURL; ?>","service":"x"} /-->
