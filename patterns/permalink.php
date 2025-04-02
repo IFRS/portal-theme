2a2ad7d823ebb19de1078d1fc7c2cc6ff81428ee
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-04-02 14:47:19 -0300

    Ajustes no template de busca, permalink não funciona como pattern

--- patterns/permalink.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Title: Permalink
- * Slug: ifrs/permalink
- * Categories: link
- * Block Types: core/paragraph
- */
-?>
-<a href="<?php echo esc_url( get_the_permalink() ); ?>" data-type="page"><?php echo get_the_permalink() ?></a>
