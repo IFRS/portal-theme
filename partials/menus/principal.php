7b693b2ce280579fe8dea8acae73a2cb2e22c924
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-04-03 14:42:32 -0300

    Ajuste nos links de início e fim de menu

--- partials/menus/principal.php
+++ partials/menus/principal.php
@@ -1,4 +1,3 @@
-<a href="#inicio-menu" id="inicio-menu" class="visually-hidden">In&iacute;cio da navega&ccedil;&atilde;o</a>
 <button class="btn btn-link p-0 mt-1 btn-menu-toggle" data-bs-toggle="collapse" data-bs-target="#portal-menu-principal" aria-expanded="false" aria-controls="portal-menu-principal" aria-label="Alternar Menu">
   <svg class="ham hamRotate" viewBox="0 0 100 100">
     <path
@@ -12,9 +11,8 @@
       d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
   </svg>
 </button>
-<?php
-  add_action( 'portal_menu', function() {
-?>
+<?php add_action( 'portal_menu', function() { ?>
+  <a href="#inicio-menu" id="inicio-menu" class="visually-hidden">In&iacute;cio da navega&ccedil;&atilde;o</a>
   <nav class="collapse menu-principal-collapse" tabindex="-1" id="portal-menu-principal" aria-label="Navegação Principal">
     <?php
       wp_nav_menu(array(
@@ -28,8 +26,5 @@
       ));
     ?>
   </nav>
-<?php
-  } );
-?>
-
-<a href="#fim-menu" id="fim-menu" class="visually-hidden">Fim da navega&ccedil;&atilde;o</a>
+  <a href="#fim-menu" id="fim-menu" class="visually-hidden">Fim da navega&ccedil;&atilde;o</a>
+<?php } );
