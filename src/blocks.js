824d02bb172e1d82f3d24f03213adcfcc3a6d449
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-05-09 15:32:18 -0300

    Criação de variante de destaque para os botões

--- src/blocks.js
+++ src/blocks.js
@@ -1,5 +1,6 @@
 /* Block Styles */
 import './blocks/styles/core-heading_separador.js'
+import './blocks/styles/core-button_destaque.js'
 
 /* Block Variations */
 import './blocks/variations/cta-banner.js'
---
+++ src/blocks/styles/core-button_destaque.js
@@ -0,0 +1,6 @@
+window.wp.domReady( () => {
+  window.wp.blocks.registerBlockStyle( 'core/button', {
+    name: 'destaque',
+    label: 'Destaque',
+  } )
+})
