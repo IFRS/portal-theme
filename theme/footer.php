13289f6541ea5e91300799399c106d2287bb6233
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-01-22 10:27:28 -0300

    Organização semântica do rodapé

--- theme/footer.php
+++ theme/footer.php
@@ -1,50 +1,51 @@
 <a href="#fim-conteudo" id="fim-conteudo" class="visually-hidden">Fim do conte&uacute;do</a>
 
-<!-- wp:template-part {"slug":"prefooter","theme":"ifrs-portal-theme","area":"prefooter","lock":{"move":true,"remove":true}} /-->
+<?php echo do_blocks('<!-- wp:template-part {"slug":"prefooter","className":"container","theme":"ifrs-portal-theme","lock":{"move":true,"remove":true}} /-->'); ?>
 
 <?php if (has_nav_menu( 'campi' )) get_template_part('partials/menus/campi'); ?>
 
-<!-- Rodapé -->
-<a href="#inicio-rodape" id="inicio-rodape" class="visually-hidden">In&iacute;cio do rodap&eacute;</a>
+<footer>
+  <a href="#inicio-rodape" id="inicio-rodape" class="visually-hidden">In&iacute;cio do rodap&eacute;</a>
 
-<section class="footer__content">
-  <div class="container">
-    <!-- wp:template-part {"slug":"footer-content"} /-->
+  <section class="footer__content">
+    <div class="container">
+      <?php echo do_blocks('<!-- wp:template-part {"slug":"footer-content"} /-->'); ?>
 
-    <div class="row align-items-center py-4">
-      <!-- Redes Sociais -->
-      <div class="col-12 col-sm-6">
-        <?php echo block_template_part( 'social' ); ?>
-      </div>
-      <!-- Acesso à Informação -->
-      <div class="col-12 col-sm-6">
-        <a href="https://www.gov.br/acessoainformacao" target="_blank" class="footer__lai ms-0 ms-sm-auto" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="Site sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o">
-          <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/acesso-info.svg' ) ); ?>" alt="Site sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o (abre uma nova p&aacute;gina)" class="lazyload img-fluid mx-auto" width="544" height="228" loading="lazy" />
-        </a>
+      <div class="row align-items-center py-4">
+        <!-- Redes Sociais -->
+        <div class="col-12 col-sm-6">
+          <?php echo block_template_part( 'social' ); ?>
+        </div>
+        <!-- Acesso à Informação -->
+        <div class="col-12 col-sm-6">
+          <a href="https://www.gov.br/acessoainformacao" target="_blank" class="footer__lai ms-0 ms-sm-auto" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="Site sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o">
+            <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/acesso-info.svg' ) ); ?>" alt="Site sobre a Lei de Acesso &agrave; Informa&ccedil;&atilde;o (abre uma nova p&aacute;gina)" class="lazyload img-fluid mx-auto" width="544" height="228" loading="lazy" />
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section class="footer__creditos">
-  <!-- Wordpress -->
-  <a href="https://br.wordpress.org/" target="_blank" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="Desenvolvido com Wordpress">
-    <img src="<?php echo esc_url (get_parent_theme_file_uri( '/img/creditos-wordpress.png' ) ); ?>" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)" class="lazyload" width="98" height="20" loading="lazy" />
-    <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
-  </a>
-  <!-- Código-fonte -->
-  <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
-    <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/creditos-git.png' ) ); ?>" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)" class="lazyload" width="43" height="18" loading="lazy" />
-    <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
-  </a>
-  <!-- Creative Commons -->
-  <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener license" data-bs-toggle="tooltip" data-bs-placement="top" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
-    <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/creditos-cc-by-nc-sa.png' ) ); ?>" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)" class="lazyload" width="80" height="15" loading="lazy" />
-    <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
-  </a>
-</section>
+  <section class="footer__creditos">
+    <!-- Wordpress -->
+    <a href="https://br.wordpress.org/" target="_blank" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="Desenvolvido com Wordpress">
+      <img src="<?php echo esc_url (get_parent_theme_file_uri( '/img/creditos-wordpress.png' ) ); ?>" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)" class="lazyload" width="98" height="20" loading="lazy" />
+      <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
+    </a>
+    <!-- Código-fonte -->
+    <a href="https://github.com/IFRS/portal-theme/" target="_blank" rel="noopener" data-bs-toggle="tooltip" data-bs-placement="top" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
+      <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/creditos-git.png' ) ); ?>" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)" class="lazyload" width="43" height="18" loading="lazy" />
+      <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
+    </a>
+    <!-- Creative Commons -->
+    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR" target="_blank" rel="noopener license" data-bs-toggle="tooltip" data-bs-placement="top" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
+      <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/creditos-cc-by-nc-sa.png' ) ); ?>" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)" class="lazyload" width="80" height="15" loading="lazy" />
+      <i class="ms-1 fa-solid fa-arrow-up-right-from-square"></i>
+    </a>
+  </section>
 
-<a href="#fim-rodape" id="fim-rodape" class="visually-hidden">Fim do rodap&eacute;</a>
+  <a href="#fim-rodape" id="fim-rodape" class="visually-hidden">Fim do rodap&eacute;</a>
+</footer>
 
 <?php wp_footer(); ?>
 
