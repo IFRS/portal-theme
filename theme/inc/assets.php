530688ceb07cb5a36fb306a830082509af899db0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-20 12:32:00 -0300

    Ajustes no carregamento de assets e uso de scripts ESM

--- theme/inc/assets.php
+++ theme/inc/assets.php
@@ -11,15 +11,15 @@ if (file_exists($manifestFile)) {
   add_action('enqueue_block_editor_assets', function() use ($manifest) {
     $block_asset_dependencies = ['wp-i18n', 'wp-blocks', 'wp-dom-ready', 'wp-data', 'wp-edit-post'];
 
-    wp_enqueue_script($manifest['src/blocks.js']['name'], get_parent_theme_file_uri( $manifest['src/blocks.js']['file'] ), $block_asset_dependencies);
+    wp_enqueue_script_module($manifest['src/blocks.js']['name'], get_parent_theme_file_uri( $manifest['src/blocks.js']['file'] ), $block_asset_dependencies);
   });
 
   /**
    * Gutenberg Content
    */
   add_action('enqueue_block_assets', function() use ($manifest) {
-    wp_enqueue_script($manifest['src/bootstrap-blocks.js']['name'], get_parent_theme_file_uri( $manifest['src/bootstrap-blocks.js']['file'] ), array());
-    wp_enqueue_style($manifest['sass/editor-styles.scss']['name'], get_parent_theme_file_uri( $manifest['sass/editor-styles.scss']['file'] ), array());
+    wp_enqueue_style($manifest['sass/editor-styles.scss']['name'], get_parent_theme_file_uri( $manifest['sass/editor-styles.scss']['file'] ));
+    wp_enqueue_script_module($manifest['src/bootstrap-blocks.js']['name'], get_parent_theme_file_uri( $manifest['src/bootstrap-blocks.js']['file'] ));
   });
 
   /**
@@ -38,37 +38,38 @@ if (file_exists($manifestFile)) {
      * wp_enqueue_style( string $handle, string $src, string[] $deps = array(), string|bool|null $ver = false, string $media )
      */
 
-    wp_enqueue_style($manifest['sass/portal.scss']['name'], get_parent_theme_file_uri($manifest['sass/portal.scss']['file']), array(), false, 'all');
+    wp_enqueue_style($manifest['sass/fonts.scss']['name'], get_parent_theme_file_uri($manifest['sass/fonts.scss']['file']), array(), null, 'all');
+    wp_enqueue_style($manifest['sass/portal.scss']['name'], get_parent_theme_file_uri($manifest['sass/portal.scss']['file']), array(), null, 'all');
 
     /* Pages */
-    wp_register_style($manifest['sass/page_front-page.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_front-page.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_home.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_home.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_search.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_search.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_single.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_single.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_page.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_page.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_concursos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_concursos.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_documentos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_documentos.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_editais.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_editais.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/page_cursos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_cursos.scss']['file']), array('portal'), false, 'all');
-    wp_register_style($manifest['sass/plugin_cursos-estude.scss']['name'], get_parent_theme_file_uri($manifest['sass/plugin_cursos-estude.scss']['file']), array('portal'), false, 'all');
+    wp_register_style($manifest['sass/page_front-page.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_front-page.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_home.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_home.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_search.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_search.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_single.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_single.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_page.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_page.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_concursos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_concursos.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_documentos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_documentos.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_editais.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_editais.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/page_cursos.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_cursos.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
+    wp_register_style($manifest['sass/plugin_cursos-estude.scss']['name'], get_parent_theme_file_uri($manifest['sass/plugin_cursos-estude.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
 
     /* DataTables */
-    wp_register_style($manifest['sass/page_datatables.scss']['name'], get_parent_theme_file_uri($manifest['sass/page_datatables.scss']['file']), array('portal'), false, 'all');
+    wp_register_style($manifest['sass/datatables.scss']['name'], get_parent_theme_file_uri($manifest['sass/datatables.scss']['file']), array($manifest['sass/portal.scss']['name']), null, 'all');
 
     /**
      * Scripts
      *
      * wp_register_script( string $handle, string|false $src, string[] $deps = array(), string|bool|null $ver = false, array|bool $args = array() ): bool
      * wp_enqueue_script( string $handle, string $src, string[] $deps = array(), string|bool|null $ver = false, array|bool $args = array() )
+     *
+     * wp_register_script_module( string $id, string $src, array $deps = array(), string|false|null $version = false, array $args = array() )
      * wp_enqueue_script_module( string $id, string $src, array $deps = array(), string|false|null $version = false, array $args = array() )
      */
 
-    wp_enqueue_script($manifest['src/portal.js']['name'], get_parent_theme_file_uri($manifest['src/portal.js']['file']), array(), false, true);
-    wp_register_script($manifest['src/datatables.js']['name'], get_parent_theme_file_uri($manifest['src/datatables.js']['file']), array('jquery'), false, true);
+    wp_enqueue_script_module($manifest['src/portal.js']['name'], get_parent_theme_file_uri($manifest['src/portal.js']['file']), array(), null, array('in_footer' => true));
 
-    if (!WP_DEBUG) {
-      wp_enqueue_script('vlibras', 'https://vlibras.gov.br/app/vlibras-plugin.js', array(), null, true);
-    }
+    /* DataTables */
+    wp_register_script_module($manifest['src/datatables.js']['name'], get_parent_theme_file_uri($manifest['src/datatables.js']['file']), array('jquery'), null, array('in_footer' => true, 'strategy' => 'async', 'fetchpriority' => 'low'));
 
     /**
      * Conditionals
@@ -144,34 +145,17 @@ if (file_exists($manifestFile)) {
       is_singular('documento') ||
       is_singular('edital')
     ) {
-      wp_enqueue_style($manifest['sass/page_datatables.scss']['name']);
-      wp_enqueue_script($manifest['src/datatables.js']['name']);
+      wp_enqueue_style($manifest['sass/datatables.scss']['name']);
+      wp_enqueue_script_module($manifest['src/datatables.js']['name']);
     }
 
     if (is_plugin_active( 'ifrs-portal-plugin-cursos-estude/portal-plugin-cursos-estude.php' )) {
-      wp_enqueue_style( $manifest['sass/page_cursos-estude.scss']['name'] );
+      wp_enqueue_style( $manifest['sass/plugin_cursos-estude.scss']['name'] );
+    }
+
+    /* VLibras */
+    if (!WP_DEBUG) {
+      wp_enqueue_script('vlibras', 'https://vlibras.gov.br/app/vlibras-plugin.js', array(), null, array('in_footer' => true, 'strategy' => 'defer', 'fetchpriority' => 'low'));
     }
   }, 1);
-
-  /**
-   * Defer e Async Scripts
-   */
-  add_filter('script_loader_tag', function($tag, $handle) use ($manifest) {
-    $scripts_to_defer = array('vlibras');
-    $scripts_to_async = array($manifest['src/datatables.js']['name']);
-
-    foreach ($scripts_to_defer as $defer_script) {
-      if ($defer_script === $handle) {
-        return str_replace(' src', ' defer="defer" src', $tag);
-      }
-    }
-
-    foreach ($scripts_to_async as $async_script) {
-      if ($async_script === $handle) {
-        return str_replace(' src', ' async="async" src', $tag);
-      }
-    }
-
-    return $tag;
-  }, 2, 2);
 }
