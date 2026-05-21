201159c58f5284148148f4fff664225f9d93a601
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-21 15:01:19 -0300

    Ajuste em JS apontado por linter

--- src/modules/tooltips.js
+++ src/modules/tooltips.js
@@ -2,3 +2,5 @@ import { Tooltip } from 'bootstrap'
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
+
+export default tooltipList
