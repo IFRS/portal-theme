f5db2800b4807052318680501c0e588fb3fd8e65
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-22 09:26:50 -0300

    Adição de espaçamento em telas menos

--- theme/header.php
+++ theme/header.php
@@ -41,7 +41,7 @@
       </div>
 
       <div class="row align-items-center mb-3">
-        <div class="col-12 col-md-6 col-lg-8 d-flex align-items-center">
+        <div class="col-12 col-md-6 col-lg-8 d-flex align-items-center mb-3 mb-md-0">
           <?php get_template_part('partials/menus/principal'); ?>
           <div class="header__principal">
             <h1 class="visually-hidden"><?php bloginfo('name'); ?></h1>
