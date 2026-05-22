86f5b436a07b945db7bc929fad9a94131e463eba
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-22 17:37:41 -0300

    Fallback em imagem destaque também na página inicial

--- theme/inc/feature-image-fallback.php
+++ theme/inc/feature-image-fallback.php
@@ -9,12 +9,12 @@ add_filter('render_block_core/post-featured-image', function($output, $block) {
   global $post;
 
   // Verifica se está dentro de um query loop (listagem) e não na página single
-  $is_query_loop = !is_singular();
+  $is_query_loop = !is_singular() || is_front_page();
 
   // Verifica se o post existe e não tem imagem destacada
   if ($is_query_loop && empty($output) && $post) {
     // Define a URL da imagem padrão
-    $fallback_image_url = get_theme_file_uri('/img/noticia-placeholder.jpg');
+    $fallback_image_url = get_theme_file_uri('/img/noticia-placeholder2.jpg');
 
     // Extrai os atributos do bloco para manter as dimensões e alinhamento
     $attributes = isset($block['attrs']) ? $block['attrs'] : array();
