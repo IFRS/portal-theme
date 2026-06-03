4a6b725cf7bf56aad665ea65e3c9c3caaf94dd9c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-03 14:24:07 -0300

    Adição de guardas e sanitização para tratar buscas vazias

--- theme/inc/empty-search-filter.php
+++ theme/inc/empty-search-filter.php
@@ -1,7 +1,23 @@
 <?php
 add_filter('pre_get_posts', function($query) {
-   if (((isset($_GET['s']) && empty($_GET['s'])) || (isset($_POST['s']) && empty($_POST['s']))) && $query->is_main_query()) {
+   if (is_admin() || !$query->is_main_query() || !$query->is_search()) {
+      return $query;
+   }
+
+   $search_get = null;
+   $search_post = null;
+
+   if (isset($_GET['s'])) {
+      $search_get = sanitize_text_field(wp_unslash($_GET['s']));
+   }
+
+   if (isset($_POST['s'])) {
+      $search_post = sanitize_text_field(wp_unslash($_POST['s']));
+   }
+
+   if (($search_get !== null && $search_get === '') || ($search_post !== null && $search_post === '')) {
       $query->is_search = false;
    }
+
    return $query;
 });
