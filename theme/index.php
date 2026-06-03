d0986652a11cdbf095cc528a6c64d06adf0813e2
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-03 14:24:56 -0300

    Adição de tag main para seguir o restante do tema

--- theme/index.php
+++ theme/index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
-<div class="alert alert-danger">
-  <p><strong>Erro!</strong>&nbsp;Template n&atilde;o encontrado.&nbsp;Tente voltar para a <a href="<?php echo esc_url( home_url() ); ?>" data-type="page">p&aacute;gina inicial</a>.</p>
-</div>
+<main class="container">
+  <div class="alert alert-danger">
+    <p><strong>Erro!</strong>&nbsp;Template n&atilde;o encontrado.&nbsp;Tente voltar para a <a href="<?php echo esc_url( home_url() ); ?>" data-type="page">p&aacute;gina inicial</a>.</p>
+  </div>
+</main>
 
 <?php get_footer(); ?>
