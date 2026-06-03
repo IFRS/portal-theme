847f952aa5c2c30c2171b2fed1496567ba2a5096
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-03 15:32:41 -0300

    Redução de repetição criando um partial para lista de notícias

--- theme/taxonomy-escopo.php
+++ theme/taxonomy-escopo.php
@@ -1,12 +1,5 @@
 <?php get_header(); ?>
 
-<main class="container">
-  <?php ob_start(); ?>
-
-  <!-- wp:query-title {"type":"archive","level":2,"showPrefix":false,"className":"mb-4"} /-->
-  <!-- wp:template-part {"slug":"noticias","lock":{"move":true,"remove":true}} /-->
-
-  <?php echo do_blocks(ob_get_clean()); ?>
-</main>
+<?php get_template_part('partials/archive-noticias'); ?>
 
 <?php get_footer(); ?>
