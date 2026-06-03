d9610cb9a6ed8dfae43e387df0a579b121cb554a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-03 17:33:38 -0300

    Reforço de confiabilidade ao carregar custom logo

--- theme/inc/resource-hints.php
+++ theme/inc/resource-hints.php
@@ -4,6 +4,9 @@ add_action('wp_head', function() {
   <link rel="preconnect" href="https://vlibras.gov.br">
 <?php
   if (has_custom_logo()) {
-    echo '<link rel="preload" href="' . esc_url(wp_get_attachment_image_src(get_theme_mod('custom_logo'), 'full')[0]) . '" as="image"/>';
+    $custom_logo = wp_get_attachment_image_src(get_theme_mod('custom_logo'), 'full');
+    if (is_array($custom_logo) && !empty($custom_logo[0])) {
+      echo '<link rel="preload" href="' . esc_url($custom_logo[0]) . '" as="image"/>';
+    }
   }
 }, 0);
