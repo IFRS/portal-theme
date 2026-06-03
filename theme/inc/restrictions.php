0fdf8cd2d89b960f96c4b73bda4760bf117200dc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-03 15:40:40 -0300

    Melhor forma de adicionar CSS personalizado na área administrativa

--- theme/inc/restrictions.php
+++ theme/inc/restrictions.php
@@ -1,16 +1,18 @@
 <?php
 // Remove os atributos de página para não-administradores
 add_action('admin_menu', function() {
-    if ( is_admin() && !current_user_can('administrator') ) {
-        remove_meta_box('pageparentdiv', 'page', 'normal');
-    }
+  if ( is_admin() && !current_user_can('administrator') ) {
+    remove_meta_box('pageparentdiv', 'page', 'normal');
+  }
 });
 
 // Esconde o metabox do Yoast SEO para não-administradores
-add_action('admin_head', function() {
-    if (!current_user_can('administrator')) {
-        echo '<style type="text/css">';
-        echo '#wpseo_meta { display: none !important; }';
-        echo '</style>';
-    }
+add_action('admin_enqueue_scripts', function() {
+  if (current_user_can('administrator')) {
+    return;
+  }
+
+  wp_register_style('portal-admin-restrictions', false);
+  wp_enqueue_style('portal-admin-restrictions');
+  wp_add_inline_style('portal-admin-restrictions', '#wpseo_meta { display: none !important; }');
 });
