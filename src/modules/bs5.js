ae8497a400b8767eccd1ab343e9495cecae61974
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-11 11:36:38 -0300

    Adição de labels ao menu social e ajuste no texto do CC

--- src/modules/bs5.js
+++ src/modules/bs5.js
@@ -4,4 +4,13 @@ import Tooltip from 'bootstrap/js/dist/tooltip'
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
 
+const socialLinks = document.querySelectorAll('.wp-block-social-link-anchor')
+socialLinks.forEach(link => {
+  let title = link.querySelector('.wp-block-social-link-label').textContent
+  new Tooltip(link, {
+    placement: 'top',
+    title: title,
+  })
+})
+
 export default tooltipList
