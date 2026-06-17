c1512c91e4967981d3f9d32c6d789c6814f0505e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-17 16:32:41 -0300

    Adição de mudança de tamanho na busca e efeitos de transição

--- theme/searchform.php
+++ theme/searchform.php
@@ -1,3 +1,3 @@
 <a id="inicio-busca" href="#inicio-busca" class="visually-hidden">In&iacute;cio da busca</a>
 <?php
-echo do_blocks('<!-- wp:search {"label":"Pesquisar","showLabel":false,"placeholder":"Digite um termo para a busca","width":100,"widthUnit":"%","buttonText":"Pesquisar","buttonPosition":"button-inside","buttonUseIcon":true} /-->');
+echo do_blocks('<!-- wp:search {"label":"Pesquisar","showLabel":false,"placeholder":"Digite um termo para a busca","buttonText":"Pesquisar","buttonPosition":"button-inside","buttonUseIcon":true} /-->');
