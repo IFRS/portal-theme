1e4fc98d30e77ea96091c16f0066f9b82e97f363
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-06-30 11:32:02 -0300

    Melhorias no texto de erro 404

--- theme/404.php
+++ theme/404.php
@@ -3,16 +3,18 @@
 <main class="container">
   <?php ob_start(); ?>
 
-  <!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","className":"img-fluid"} -->
-  <figure class="wp-block-image aligncenter size-full img-fluid"><img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/404.png' ) ); ?>" alt="Erro 404 - Página não encontrada" loading="eager" /></figure>
+  <!-- wp:image {"scale":"cover","sizeSlug":"full","lightbox":{"enabled":false},"linkDestination":"none","align":"center","className":"img-fluid"} -->
+  <figure class="wp-block-image aligncenter size-full img-fluid">
+    <img src="<?php echo esc_url( get_parent_theme_file_uri( '/img/404.png' ) ); ?>" alt="Erro 404 - Página não encontrada" loading="eager" />
+  </figure>
   <!-- /wp:image -->
 
   <!-- wp:heading {"textAlign":"center"} -->
-  <h2 class="wp-block-heading has-text-align-center">Ops, temos um problema!</h2>
+  <h2 class="wp-block-heading has-text-align-center">N&atilde;o foi possível localizar esta p&aacute;gina!</h2>
   <!-- /wp:heading -->
 
   <!-- wp:paragraph {"align":"center"} -->
-  <p class="has-text-align-center">A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url( home_url() ); ?>" data-type="page">p&aacute;gina inicial</a>.</p>
+  <p class="has-text-align-center">Verifique o endere&ccedil;o informado ou tente voltar para a <a href="<?php echo esc_url( home_url() ); ?>" data-type="page">p&aacute;gina inicial</a>.</p>
   <!-- /wp:paragraph -->
 
   <?php echo do_blocks(ob_get_clean()); ?>
