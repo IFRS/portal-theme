6ebb2c0ccd7ef4ad9ec10d49f4ea75f13669fda3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-07-02 17:13:21 -0300

    Reformulação do menu de subpáginas

--- src/modules/bs5.js
+++ src/modules/bs5.js
@@ -1,12 +1,14 @@
-import 'bootstrap/js/dist/collapse'
-import 'bootstrap/js/dist/offcanvas'
 import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/offcanvas'
 import 'bootstrap/js/dist/tab'
+import Collapse from 'bootstrap/js/dist/collapse'
 import Tooltip from 'bootstrap/js/dist/tooltip'
 
+/* Initialize tooltips */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
 
+/* Initialize tooltips for social links */
 const socialLinks = document.querySelectorAll('.wp-block-social-link-anchor')
 socialLinks.forEach(link => {
   let title = link.querySelector('.wp-block-social-link-label').textContent
@@ -17,4 +19,25 @@ socialLinks.forEach(link => {
   })
 })
 
-export default tooltipList
+/* Collapse subpages based on viewport width */
+const collapseSubpages = document.querySelectorAll('.subpages > .collapse')
+
+const belowMd = window.matchMedia('(max-width: 767.98px)')
+
+const syncCollapseWithBreakpoint = event => {
+  collapseSubpages.forEach(collapseEl => {
+    const collapse = Collapse.getOrCreateInstance(collapseEl)
+    if (event.matches) {
+      collapse.hide()
+      return
+    }
+
+    collapse.show()
+  })
+}
+
+syncCollapseWithBreakpoint(belowMd)
+belowMd.addEventListener('change', syncCollapseWithBreakpoint)
+
+
+export default { tooltipList, socialLinks, collapseSubpages }
--- theme/patterns/subpages.php
+++ theme/patterns/subpages.php
@@ -19,22 +19,29 @@ $children = get_pages(
 $parent = wp_get_post_parent_id( $ID );
 $ancestors = get_post_ancestors( $ID );
 $depth = count($ancestors);
+
+$collapseID = wp_unique_id('subpages-collapse-');
 ?>
 
 <?php if (count($children) > 0) : ?>
-  <ul class="nav subpages">
-  <?php if ($parent) : ?>
-    <li class="nav-item nav-item--parent">
-      <a class="nav-link" href="<?php echo esc_url(get_page_link($parent)); ?>">
-        <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 640 640" fill="currentColor" class="me-1"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M160 512C142.3 512 128 526.3 128 544C128 561.7 142.3 576 160 576L256 576C309 576 352 533 352 480L352 173.3L425.4 246.7C437.9 259.2 458.2 259.2 470.7 246.7C483.2 234.2 483.2 213.9 470.7 201.4L342.7 73.4C330.2 60.9 309.9 60.9 297.4 73.4L169.4 201.4C156.9 213.9 156.9 234.2 169.4 246.7C181.9 259.2 202.2 259.2 214.7 246.7L288 173.3L288 480C288 497.7 273.7 512 256 512L160 512z"/></svg>
-        <?php echo esc_html(get_the_title($parent)); ?>
-      </a>
-    </li>
-  <?php endif; ?>
-  <?php foreach ($children as $child): ?>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo esc_url(get_page_link($child->ID)); ?>"><?php echo esc_html($child->post_title); ?></a>
-    </li>
-  <?php endforeach; ?>
-  </ul>
+  <nav class="subpages">
+    <button class="btn subpages__toggle" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $collapseID; ?>" aria-expanded="false" aria-controls="<?php echo $collapseID; ?>">
+      Menu da P&aacute;gina
+    </button>
+    <ul class="collapse show subpages__menu" id="<?php echo $collapseID; ?>">
+    <?php if ($parent) : ?>
+      <li class="subpages__item subpages__item--parent">
+        <a class="subpages__link" href="<?php echo esc_url(get_page_link($parent)); ?>">
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 640 640" fill="currentColor" class="me-1"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M160 512C142.3 512 128 526.3 128 544C128 561.7 142.3 576 160 576L256 576C309 576 352 533 352 480L352 173.3L425.4 246.7C437.9 259.2 458.2 259.2 470.7 246.7C483.2 234.2 483.2 213.9 470.7 201.4L342.7 73.4C330.2 60.9 309.9 60.9 297.4 73.4L169.4 201.4C156.9 213.9 156.9 234.2 169.4 246.7C181.9 259.2 202.2 259.2 214.7 246.7L288 173.3L288 480C288 497.7 273.7 512 256 512L160 512z"/></svg>
+          <?php echo esc_html(get_the_title($parent)); ?>
+        </a>
+      </li>
+    <?php endif; ?>
+    <?php foreach ($children as $child): ?>
+      <li class="subpages__item">
+        <a class="subpages__link" href="<?php echo esc_url(get_page_link($child->ID)); ?>"><?php echo esc_html($child->post_title); ?></a>
+      </li>
+    <?php endforeach; ?>
+    </ul>
+  </nav>
 <?php endif; ?>
